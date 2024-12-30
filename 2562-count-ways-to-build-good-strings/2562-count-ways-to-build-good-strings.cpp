4cfe6434c4c75c8c2169f3c14d61876bfce19ba3
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2024-12-30 14:28:02 +0200

    Time: 11 ms (24.66%), Space: 15.6 MB (46.58%) - LeetHub

--- 2562-count-ways-to-build-good-strings/2562-count-ways-to-build-good-strings.cpp
+++ 2562-count-ways-to-build-good-strings/2562-count-ways-to-build-good-strings.cpp
@@ -7,22 +7,22 @@ public:
         dp[0] = 1;
 
         int ans = 0;
-        for(int end = low; end <= high; end++){
+        for (int end = low; end <= high; end++) {
             ans += dfs(end, zero, one);
             ans %= mod;
         }
         return ans;
     }
 
-    int dfs(int end, int zero, int one){
+    int dfs(int end, int zero, int one) {
         int count = 0;
-        if(dp[end] != -1){
+        if (dp[end] != -1) {
             return dp[end];
         }
-        if(end >= zero){
+        if (end >= zero) {
             count += dfs(end - zero, zero, one);
         }
-        if(end >= one){
+        if (end >= one) {
             count += dfs(end - one, zero, one);
         }
         return dp[end] = count % mod;
