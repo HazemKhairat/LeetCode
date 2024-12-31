c566c5755e2061427f7079d4e531a8d4554b2245
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2024-12-31 10:00:18 +0200

    Time: 3 ms (26.6%), Space: 13.4 MB (32.35%) - LeetHub

---
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    map<int, int> memo;
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        return dfs(days, costs, 0);
+    }
+
+    int findNextIndex(const std::vector<int>& days, int value) {
+        auto it = upper_bound(days.begin(), days.end(), value);
+        return distance(days.begin(), it);
+    }
+
+    int dfs(vector<int>& days, vector<int>& costs, int idx) {
+        int nextIdx = 0;
+        if (idx >= days.size()) {
+            return 0;
+        }
+        if (memo.count(idx))
+            return memo[idx];
+        int One = costs[0] + dfs(days, costs, idx + 1);
+        nextIdx = findNextIndex(days, days[idx] + 7 - 1);
+        int Seven = costs[1] + dfs(days, costs, nextIdx);
+        nextIdx = findNextIndex(days, days[idx] + 30 - 1);
+        int Thirty = costs[2] + dfs(days, costs, nextIdx);
+        return memo[idx] = min({One, Seven, Thirty});
+    }
+};
