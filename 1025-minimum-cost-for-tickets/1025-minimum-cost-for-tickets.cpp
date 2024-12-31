2f4a854c64c9d817557b3ca977ad35bb9a11e05c
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2024-12-31 17:56:22 +0200

    Time: 0 ms (100%), Space: 12.9 MB (59.41%) - LeetHub

--- 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
-    map<int, int> memo;
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-        return dfs(days, costs, 0);
-    }
+        int maxDay = days.back();
+        vector<int> allDays(maxDay + 1);
 
-    int findNextIndex(vector<int>& days, int value) {
-        auto it = upper_bound(days.begin(), days.end(), value);
-        return distance(days.begin(), it);
-    }
-
-    int dfs(vector<int>& days, vector<int>& costs, int idx) {
-        int nextIdx = 0;
-        if (idx >= days.size()) {
-            return 0;
+        int idx = 0;
+        for (int i = 1; i <= maxDay; i++) {
+            if (i < days[idx]) {
+                allDays[i] = allDays[i - 1];
+            } 
+            else {
+                idx++;
+                allDays[i] = min({
+                    allDays[i - 1] + costs[0],
+                    allDays[max(0, i - 7)] + costs[1],
+                    allDays[max(0, i - 30)] + costs[2],
+                });
+            }
         }
-        if (memo.count(idx))
-            return memo[idx];
-        int One = costs[0] + dfs(days, costs, idx + 1);
-        nextIdx = findNextIndex(days, days[idx] + 7 - 1);
-        int Seven = costs[1] + dfs(days, costs, nextIdx);
-        nextIdx = findNextIndex(days, days[idx] + 30 - 1);
-        int Thirty = costs[2] + dfs(days, costs, nextIdx);
-        return memo[idx] = min({One, Seven, Thirty});
+
+        return allDays[maxDay];
     }
 };
