bb416c8cfb64adcbc51487a7d7971e96e50f45cc
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-01-05 11:29:34 +0200

    Time: 407 ms (33.44%), Space: 16.5 MB (34.37%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
@@ -2,22 +2,23 @@ class Solution {
 public:
     int countPalindromicSubsequence(string s) {
         set<char> chars(s.begin(), s.end());
-        map<char, int> firstPos, lastPos;
-        for (int i = 0; i < s.size(); i++) {
-            if (!firstPos.count(s[i])) {
-                firstPos[s[i]] = i;
-            }
-            lastPos[s[i]] = i;
-        }
-
         int ans = 0;
         for (auto ch : chars) {
-            int start = firstPos[ch] + 1, end = lastPos[ch] - 1;
-            set<char> midChars;
-            for (int i = start; i <= end; i++) {
-                midChars.insert(s[i]);
+            int firstPos = -1, lastPos = -1;
+            for (int i = 0; i < s.size(); i++) {
+                if (ch == s[i]) {
+                    if (firstPos == -1) {
+                        firstPos = i;
+                    }
+                    lastPos = i;
+                }
             }
-            ans += midChars.size();
+
+            set<char> mid;
+            for (int k = firstPos + 1; k < lastPos; k++) {
+                mid.insert(s[k]);
+            }
+            ans += mid.size();
         }
 
         return ans;
