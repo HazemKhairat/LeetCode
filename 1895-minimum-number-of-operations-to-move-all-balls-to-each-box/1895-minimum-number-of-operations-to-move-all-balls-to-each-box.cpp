edbaa08bff069cf299f09aab97cbf898410adab9
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-01-06 10:00:04 +0200

    Time: 1 ms (68.94%), Space: 12.2 MB (40.56%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     vector<int> minOperations(string boxes) {
-        vector<int> res(boxes.size());
-        for (int i = 0; i < boxes.size(); i++) {
-            int ans = 0;
-            for (int j = 0; j < boxes.size(); j++) {
-                if (boxes[j] == '1') {
-                    ans += abs(j - i);
-                }
-            }
-            res[i] = ans;
+        int n = boxes.size();
+        vector<int> ans(n);
+        int leftBalls = 0, movesToLeft = 0;
+        int rightBalls = 0, movesToRight = 0;
+
+        for(int i = 0; i < n; i++){
+            ans[i] += movesToLeft;
+            leftBalls += boxes[i] - '0';
+            movesToLeft += leftBalls;
+
+            int j = n - 1 - i;
+            ans[j] += movesToRight;
+            rightBalls += boxes[j] - '0';
+            movesToRight += rightBalls;
         }
-        return res;
+        
+        return ans;
     }
 };
