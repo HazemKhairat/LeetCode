2cf0f493e2c67cc06bf7c3619c78bf03064b673d
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-01-07 10:39:56 +0200

    Time: 8 ms (14.58%), Space: 12.8 MB (13.84%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.cpp
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.cpp
@@ -8,12 +8,27 @@ public:
             for (int j = 0; j < n; j++) {
                 if (j == i)
                     continue;
-                if (words[j].find(text) != string::npos) {
+                if (isSubstringOf(text, words[j])) {
                     res.push_back(text);
                     break;
                 }
             }
         }
         return res;
+    }
+
+    bool isSubstringOf(string subStr, string str) {
+        for (int i = 0; i < str.size(); i++) {
+            bool match = true;
+            for (int j = 0; j < subStr.size(); j++) {
+                if (i + j >= str.size() || str[i + j] != subStr[j]) {
+                    match = false;
+                    break;
+                }
+            }
+            if (match)
+                return match;
+        }
+        return false;
     }
 };
