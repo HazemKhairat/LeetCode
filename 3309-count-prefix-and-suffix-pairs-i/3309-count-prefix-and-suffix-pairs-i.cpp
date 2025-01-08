16cec919a50210d72f529443fe873f93c5ee05a6
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-01-08 11:43:36 +0200

    Time: 19 ms (8.9%), Space: 45.9 MB (5.08%) - LeetHub

--- 3309-count-prefix-and-suffix-pairs-i/3309-count-prefix-and-suffix-pairs-i.cpp
+++ 3309-count-prefix-and-suffix-pairs-i/3309-count-prefix-and-suffix-pairs-i.cpp
@@ -1,28 +1,71 @@
+class Node {
+public:
+    Node* links[26] = {};
+
+    bool contains(char ch) { return links[ch - 'a'] != NULL; }
+
+    void put(char ch, Node* node) { links[ch - 'a'] = node; }
+
+    Node* next(char ch) { return links[ch - 'a']; }
+};
+
+class Trie {
+public:
+    Node* root;
+    Trie() { root = new Node(); }
+
+    void insert(string& word) {
+        Node* node = root;
+        for (char ch : word) {
+            if (!node->contains(ch)) {
+                node->put(ch, new Node());
+            }
+            node = node->next(ch);
+        }
+    }
+
+    bool startWith(string& prefix) {
+        Node* node = root;
+        for (auto ch : prefix) {
+            if (!node->contains(ch)) {
+                return false;
+            }
+            node = node->next(ch);
+        }
+        return true;
+    }
+};
+
 class Solution {
 public:
     int countPrefixSuffixPairs(vector<string>& words) {
-        int res = 0;
         int n = words.size();
+        int count = 0;
+
         for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (isPrefixAndSuffix(words[i], words[j])) {
-                    res++;
+            Trie prefixTrie, suffixTire;
+
+            prefixTrie.insert(words[i]);
+            string revWord = words[i];
+            reverse(revWord.begin(), revWord.end());
+            suffixTire.insert(revWord);
+
+            for (int j = 0; j < i; j++) {
+                if (words[j].size() > words[i].size()) {
+                    continue;
+                }
+
+                string prefixWord = words[j];
+                string revPrefixWord = prefixWord;
+                reverse(revPrefixWord.begin(), revPrefixWord.end());
+
+                if (prefixTrie.startWith(prefixWord) &&
+                    suffixTire.startWith(revPrefixWord)) {
+                    count++;
                 }
             }
         }
-        return res;
-    }
 
-    bool isPrefixAndSuffix(string str1, string str2) {
-        int n = str2.size() - 1;
-        int i = 0, j = str1.size() - 1;
-        while (i < str1.size()) {
-            if (str1[i] != str2[i] || str1[j] != str2[n - i]) {
-                return false;
-            }
-            i++;
-            j--;
-        }
-        return true;
+        return count;
     }
 };
