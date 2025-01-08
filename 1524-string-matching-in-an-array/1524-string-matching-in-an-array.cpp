0bf2e795a1f33530d6782b640b2d529212c0a002
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-01-08 08:51:26 +0200

    Time: 0 ms (100%), Space: 11.6 MB (39.33%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.cpp
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.cpp
@@ -2,33 +2,16 @@ class Solution {
 public:
     vector<string> stringMatching(vector<string>& words) {
         vector<string> res;
-        int n = words.size();
-        for (int i = 0; i < n; i++) {
-            string text = words[i];
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < words.size(); i++) {
+            for (int j = 0; j < words.size(); j++) {
                 if (j == i)
                     continue;
-                if (isSubstringOf(text, words[j])) {
-                    res.push_back(text);
+                if (words[j].find(words[i]) != string::npos) {
+                    res.push_back(words[i]);
                     break;
                 }
             }
         }
         return res;
-    }
-
-    bool isSubstringOf(string subStr, string str) {
-        for (int i = 0; i < str.size(); i++) {
-            bool match = true;
-            for (int j = 0; j < subStr.size(); j++) {
-                if (i + j >= str.size() || str[i + j] != subStr[j]) {
-                    match = false;
-                    break;
-                }
-            }
-            if (match)
-                return match;
-        }
-        return false;
     }
 };
