39f34db5568d829869e0ed123db1aba8ce25e7a5
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-01-09 07:29:12 +0200

    Time: 45 ms (5.04%), Space: 110.5 MB (5.04%) - LeetHub

--- 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.cpp
+++ 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.cpp
@@ -1,13 +1,53 @@
+class Node {
+public:
+    Node* chars[26] = {};
+
+    void put(char ch) { chars[ch - 'a'] = new Node(); }
+
+    bool contain(char ch) { return chars[ch - 'a'] != NULL; }
+
+    Node* next(char ch) { return chars[ch - 'a']; }
+};
+
+class Trie {
+public:
+    Node* root;
+    Trie() { root = new Node(); }
+
+    void insert(string& word) {
+        Node* node = root;
+        for (auto ch : word) {
+            if (!node->contain(ch)) {
+                node->put(ch);
+            }
+            node = node->next(ch);
+        }
+    }
+
+    bool isPrefix(string& pref) {
+        Node* node = root;
+        for (auto ch : pref) {
+            if (!node->contain(ch)) {
+                return false;
+            }
+            node = node->next(ch);
+        }
+        return true;
+    }
+};
+
 class Solution {
 public:
     int prefixCount(vector<string>& words, string pref) {
-        int n = pref.size();
         int count = 0;
         for (auto word : words) {
-            if (word.substr(0, n) == pref) {
+            Trie prefTrie;
+            prefTrie.insert(word);
+            if (prefTrie.isPrefix(pref)) {
                 count++;
             }
         }
+
         return count;
     }
 };
