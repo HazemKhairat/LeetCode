6807c1356b98ea596251daeb54a44717f685a5de
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-01-10 21:25:10 +0200

    Time: 55 ms (62.1%), Space: 106.9 MB (33.79%) - LeetHub

--- 0952-word-subsets/0952-word-subsets.cpp
+++ 0952-word-subsets/0952-word-subsets.cpp
@@ -2,32 +2,32 @@ class Solution {
 public:
     vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
         vector<string> res;
-        vector<int> v2(26);
+        vector<int> v1(26, 0);
         for (auto word : words2) {
             vector<int> tmp(26);
             for (auto ch : word) {
                 tmp[ch - 'a']++;
-                v2[ch - 'a'] = max(v2[ch - 'a'], tmp[ch - 'a']);
+                v1[ch - 'a'] = max(v1[ch - 'a'], tmp[ch - 'a']);
             }
         }
 
         for (auto word : words1) {
-            vector<int> v1(26);
+            vector<int> v2(26);
             for (auto ch : word) {
-                v1[ch - 'a']++;
+                v2[ch - 'a']++;
             }
-            bool valid = true;
-            for (int i = 0; i < 26; i++) {
-                if (v1[i] < v2[i]) {
-                    valid = false;
+
+            bool ok = true;
+            for (int i = 0; i < v2.size(); i++) {
+                if (v2[i] < v1[i]) {
+                    ok = false;
                     break;
                 }
             }
-
-            if (valid)
+            if (ok) {
                 res.push_back(word);
+            }
         }
-
         return res;
     }
 };
