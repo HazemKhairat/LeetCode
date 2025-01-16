6b4f01babee0fcafa98f7fa0f4c17b624be3c60e
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-01-16 17:12:52 +0200

    Time: 128 ms (5.28%), Space: 90.9 MB (5.28%) - LeetHub

--- 2533-bitwise-xor-of-all-pairings/2533-bitwise-xor-of-all-pairings.cpp
+++ 2533-bitwise-xor-of-all-pairings/2533-bitwise-xor-of-all-pairings.cpp
@@ -2,29 +2,27 @@ class Solution {
 public:
     int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size(), m = nums2.size();
-        map<long long, long long> freq;
+        map<int, long long> freq;
 
-        for(int i = 0; i < n; i++){
-            freq[nums1[i]] += m;
+        for (auto num : nums1) {
+            freq[num] += m;
         }
-        for(int i = 0; i < m; i++){
-            freq[nums2[i]] += n;
+        for (auto num : nums2) {
+            freq[num] += n;
         }
 
-        vector<long long> nums3;
-        for(auto item : freq){
-            if(item.second % 2 == 1){
+        vector<int> nums3;
+        for (auto item : freq) {
+            if (item.second % 2 == 1) {
                 nums3.push_back(item.first);
             }
         }
 
         int res = 0;
-        for(auto item : nums3){
+        for (auto item : nums3) {
             res ^= item;
         }
 
         return res;
-
-
     }
 };
