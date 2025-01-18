94c3776b8a7d3cf226dfbe04d5d26ed6d641917c
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-01-18 14:16:21 +0200

    Time: 66 ms (30.8%), Space: 41 MB (16.74%) - LeetHub

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -1,34 +1,31 @@
 class Solution {
-public: //   0       1       2        3
+public: //   0       1        2        3
     vector<vector<int>> dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-
     int minCost(vector<vector<int>>& grid) {
         int n = grid.size(), m = grid[0].size();
-        priority_queue<vector<int>, vector<vector<int>>, greater<>> pq;
-        pq.push({0, 0, 0}); // cost, row, col
-            vector<vector<int>>
-                minCost(n, vector<int>(m, INT_MAX));
+        deque<vector<int>> dq;
+        dq.push_front({0, 0, 0}); // cost , row, col
+        vector<vector<int>> minCost(n, vector<int>(m, INT_MAX));
         minCost[0][0] = 0;
 
-        while (!pq.empty()) {
-            vector<int> curr = pq.top(); // [0, 0, 0]
-                               pq.pop();
-
+        while (!dq.empty()) {
+            vector<int> curr = dq.front();
+            dq.pop_front();
             int cost = curr[0], row = curr[1], col = curr[2];
-
-            if (minCost[row][col] < cost) {
-                continue;
-            }
 
             for (int dir = 0; dir < 4; dir++) {
                 int newRow = row + dirs[dir][0];
                 int newCol = col + dirs[dir][1];
 
                 if (newRow >= 0 && newCol >= 0 && newRow < n && newCol < m) {
-                    int newCost = cost + (dir != grid[row][col] - 1 ? 1 : 0);
-                    if (minCost[newRow][newCol] > newCost) {
+                    int newCost = cost + (grid[row][col] - 1 != dir);
+                    if (newCost < minCost[newRow][newCol]) {
+                        if (newCost == cost) {
+                            dq.push_front({newCost, newRow, newCol});
+                        } else {
+                            dq.push_back({newCost, newRow, newCol});
+                        }
                         minCost[newRow][newCol] = newCost;
-                        pq.push({newCost, newRow, newCol});
                     }
                 }
             }
