45456e5a505d3575667f564b9db5e0f59ea983a9
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-01-18 12:09:48 +0200

    Time: 98 ms (24.75%), Space: 30.8 MB (23.91%) - LeetHub

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -1,29 +1,32 @@
 class Solution {
-public:
+public: //   0       1       2        3
     vector<vector<int>> dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
     int minCost(vector<vector<int>>& grid) {
         int n = grid.size(), m = grid[0].size();
-        priority_queue<vector<int>, vector<vector<int>> , greater<>> pq;
-        pq.push({0, 0, 0}); // row, col, cost
-        vector<vector<int>> minCost(n, vector<int>(m, INT_MAX));
+        priority_queue<vector<int>, vector<vector<int>>, greater<>> pq;
+        pq.push({0, 0, 0}); // cost, row, col
+            vector<vector<int>>
+                minCost(n, vector<int>(m, INT_MAX));
         minCost[0][0] = 0;
 
-        while(!pq.empty()){
-            vector<int> curr = pq.top();
-            pq.pop();
+        while (!pq.empty()) {
+            vector<int> curr = pq.top(); // [0, 0, 0]
+                               pq.pop();
+
             int cost = curr[0], row = curr[1], col = curr[2];
 
-            if(minCost[row][col] != cost) continue;
+            if (minCost[row][col] < cost) {
+                continue;
+            }
 
-            for(int dir = 0; dir < 4; dir++){
+            for (int dir = 0; dir < 4; dir++) {
                 int newRow = row + dirs[dir][0];
                 int newCol = col + dirs[dir][1];
 
-                if(newRow >= 0 && newRow < n && newCol >= 0 && newCol < m){
-                    int newCost = cost + (dir != (grid[row][col] - 1) ? 1 : 0);
-
-                    if(minCost[newRow][newCol] > newCost){
+                if (newRow >= 0 && newCol >= 0 && newRow < n && newCol < m) {
+                    int newCost = cost + (dir != grid[row][col] - 1 ? 1 : 0);
+                    if (minCost[newRow][newCol] > newCost) {
                         minCost[newRow][newCol] = newCost;
                         pq.push({newCost, newRow, newCol});
                     }
@@ -32,6 +35,5 @@ public:
         }
 
         return minCost[n - 1][m - 1];
-
     }
 };
