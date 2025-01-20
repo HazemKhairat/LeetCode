be660d3863af550b3351c32b77cac5c087ac2a91
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-01-20 11:35:31 +0200

    Time: 103 ms (27.9%), Space: 29 MB (29.7%) - LeetHub

--- 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.cpp
+++ 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int trapRainWater(vector<vector<int>>& heightMap) {
-        vector<vector<int>> dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        vector<vector<int>> dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
         int n = heightMap.size(), m = heightMap[0].size();
-
-        vector<vector<bool>> visited(n, vector<bool>(m, false));
         priority_queue<vector<int>, vector<vector<int>>, greater<>> pq;
+        vector<vector<bool>> visited(n, vector<bool>(m, false));
+
         for (int i = 0; i < n; i++) {
             pq.push({heightMap[i][0], i, 0});
             pq.push({heightMap[i][m - 1], i, m - 1});
@@ -19,28 +19,26 @@ public:
         }
 
         int res = 0;
-
         while (!pq.empty()) {
             auto curr = pq.top();
             pq.pop();
-
             int hight = curr[0], row = curr[1], col = curr[2];
 
             for (int dir = 0; dir < 4; dir++) {
                 int newRow = row + dirs[dir][0];
                 int newCol = col + dirs[dir][1];
+
                 if (newRow < 0 || newCol < 0 || newRow == n || newCol == m ||
                     visited[newRow][newCol]) {
                     continue;
                 }
 
-                int neighborHight = heightMap[newRow][newCol];
-
-                if (neighborHight < hight) {
-                    res += (hight - neighborHight);
-                    neighborHight = hight;
+                if (hight > heightMap[newRow][newCol]) {
+                    res += (hight - heightMap[newRow][newCol]);
+                    heightMap[newRow][newCol] = hight;
                 }
-                pq.push({neighborHight, newRow, newCol});
+
+                pq.push({heightMap[newRow][newCol], newRow, newCol});
                 visited[newRow][newCol] = true;
             }
         }
