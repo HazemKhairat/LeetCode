c793861be69c8c2a2e852fabc40387fbd434cc06
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-01-20 10:35:57 +0200

    Time: 3 ms (97.87%), Space: 134.1 MB (82.62%) - LeetHub

--- 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.cpp
+++ 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
         int m = mat.size(), n = mat[0].size();
-        vector<pair<int, int>> mp((n * m) + 10);
+        vector<pair<int, int>> position((n * m) + 10);
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-                mp[mat[i][j]] = {i, j};
+                position[mat[i][j]] = {i, j};
             }
         }
 
         vector<int> rows(m, -1), cols(n, -1);
         for(int i = 0; i < arr.size(); i++){
-            int row = mp[arr[i]].first;
-            int col = mp[arr[i]].second;
+            int row = position[arr[i]].first;
+            int col = position[arr[i]].second;
             rows[row]++;
             cols[col]++;
             if(rows[row] == n - 1 || cols[col] == m - 1){
