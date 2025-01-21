1d08d0787217f9650405237a519a3a773ab969b0
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-01-21 17:03:46 +0200

    Time: 9 ms (19.46%), Space: 75.7 MB (23.98%) - LeetHub

--- 2145-grid-game/2145-grid-game.cpp
+++ 2145-grid-game/2145-grid-game.cpp
@@ -2,16 +2,20 @@ class Solution {
 public:
     long long gridGame(vector<vector<int>>& grid) {
         int n = grid[0].size();
-        long long topSum = accumulate(grid[0].begin(), grid[0].end(), 0LL), bottomSum = 0;
-        long long ans = LLONG_MAX;
-
-        for(int i = 0; i < n; i++){
-            topSum -= grid[0][i];
-            ans = min(ans, max(topSum, bottomSum));
-            bottomSum += grid[1][i];
+        vector<long long> pref(grid[0].begin(), grid[0].end()), suff(grid[1].begin(), grid[1].end());
+        for (int i = 1; i < n; i++) {
+            pref[i] += pref[i - 1];
+            suff[n - i - 1] += suff[n - i];
         }
 
-        return ans;
+        int col = 0;
+        long long res = LLONG_MAX;
+        for (int col = 0; col < n; col++) {
+            long long top = pref[n - 1] - pref[col];
+            long long bottom = suff[0] - suff[col];
+            res = min(res, max(top, bottom));
+        }
 
+        return res;
     }
 };
