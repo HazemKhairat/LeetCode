fe39894b7d54dcace339c18806718bdedff75d05
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-01-26 14:51:50 +0200

    Time: 956 ms (5.4%), Space: 532.7 MB (5.4%) - LeetHub

--- 3219-make-lexicographically-smallest-array-by-swapping-elements/3219-make-lexicographically-smallest-array-by-swapping-elements.cpp
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/3219-make-lexicographically-smallest-array-by-swapping-elements.cpp
@@ -3,29 +3,28 @@ public:
     vector<int> lexicographicallySmallestArray(vector<int>& nums, int limit) {
         vector<int> sortedNums(nums.begin(), nums.end());
         sort(sortedNums.begin(), sortedNums.end());
-        map<int, int> map;
+
         vector<queue<int>> groups;
-        int qNum = 0;
+        map<int, int> map;
+        int qIndex = 0;
 
         for (int i = 0; i < nums.size();) {
             queue<int> q;
             while (i < nums.size() &&
                    (q.empty() || sortedNums[i] - q.back() <= limit)) {
                 q.push(sortedNums[i]);
-                map[sortedNums[i]] = qNum;
+                map[sortedNums[i]] = qIndex;
                 i++;
             }
             groups.push_back(q);
-            qNum++;
+            qIndex++;
         }
 
-        vector<int> res(nums.size(), 0);
         for (int i = 0; i < nums.size(); i++) {
-            int groupNum = map[nums[i]];
-            res[i] = groups[groupNum].front();
-            groups[groupNum].pop();
+            nums[i] = groups[map[nums[i]]].front();
+            groups[map[nums[i]]].pop();
         }
 
-        return res;
+        return nums;
     }
 };
