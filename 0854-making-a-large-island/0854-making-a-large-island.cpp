1e70478214d9fe295702c00ec3ce93bac8346db5
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-02-02 14:58:58 +0200

    Time: 1501 ms (5.01%), Space: 406.4 MB (5.03%) - LeetHub

--- 0854-making-a-large-island/0854-making-a-large-island.cpp
+++ 0854-making-a-large-island/0854-making-a-large-island.cpp
@@ -3,35 +3,33 @@ public:
     vector<vector<int>> dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
     int largestIsland(vector<vector<int>>& grid) {
         int n = grid.size();
-        int landNum = 2;
-        unordered_map<int, int> islands;
-        for (int r = 0; r < n; r++) {
-            for (int c = 0; c < n; c++) {
-                if (grid[r][c] == 1) {
-                    int res = dfs(grid, r, c, landNum);
-                    islands[landNum] = res;
-                    // cout << landNum << " => " << islands[landNum] << endl;
-                    landNum++;
+        int land = 2;
+        map<int, int> mp;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    // cout << dfs(grid, i, j, n, land) << endl;
+                    mp[land] = dfs(grid, i, j, n, land);
+                    land++;
                 }
             }
         }
 
         int res = 0;
-        for (auto& item : islands) {
+        for (auto item : mp) {
             res = max(res, item.second);
         }
-
-        for (int r = 0; r < n; r++) {
-            for (int c = 0; c < n; c++) {
-                if (grid[r][c] == 0) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                set<int> visited;
+                if (grid[i][j] == 0) {
                     int tmp = 1;
-                    unordered_set<int> visited;
                     for (auto dir : dirs) {
-                        int nr = r + dir[0];
-                        int nc = c + dir[1];
+                        int nr = i + dir[0];
+                        int nc = j + dir[1];
                         if (!outOfBound(nr, nc, n) &&
                             !visited.count(grid[nr][nc])) {
-                            tmp += islands[grid[nr][nc]];
+                            tmp += mp[grid[nr][nc]];
                             visited.insert(grid[nr][nc]);
                         }
                     }
@@ -39,25 +37,25 @@ public:
                 }
             }
         }
-
         return res;
     }
 
-    int dfs(vector<vector<int>>& grid, int r, int c, int land) {
-        if (outOfBound(r, c, grid.size()) || grid[r][c] != 1) {
+    int dfs(vector<vector<int>>& grid, int i, int j, int n, int land) {
+        if (outOfBound(i, j, n) || grid[i][j] != 1) {
             return 0;
         }
-        grid[r][c] = land;
+
+        grid[i][j] = land;
         int res = 1;
         for (auto dir : dirs) {
-            int nr = r + dir[0];
-            int nc = c + dir[1];
-            res += dfs(grid, nr, nc, land);
+            int nr = i + dir[0];
+            int nc = j + dir[1];
+            res += dfs(grid, nr, nc, n, land);
         }
         return res;
     }
 
-    bool outOfBound(int r, int c, int n) {
-        return (r < 0 || c < 0 || r == n || c == n);
+    bool outOfBound(int i, int j, int n) {
+        return i < 0 || j < 0 || i == n || j == n;
     }
 };
