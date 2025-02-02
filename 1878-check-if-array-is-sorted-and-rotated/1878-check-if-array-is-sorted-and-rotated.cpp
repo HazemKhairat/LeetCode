a1ca42737d2a7055454b7ef157793ddca322649d
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-02-02 10:10:11 +0200

    Time: 0 ms (100%), Space: 11.2 MB (58.89%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
@@ -2,19 +2,21 @@ class Solution {
 public:
     bool check(vector<int>& nums) {
         int n = nums.size();
-        int first = 0;
+        int firstIntersection = 0;
+
         for (int i = 0; i < nums.size(); i++) {
-            cout << first << endl;
-            if (first == 1 && i == n - 1) {
+            // cout << firstIntersection << endl;
+            if (firstIntersection == 1 && i == n - 1) {
                 if (nums[i] > nums[0]) {
                     return false;
                 }
-            } else if (first > 1) {
+            } else if (firstIntersection > 1) {
                 return false;
             } else if (i < n - 1 && nums[i] > nums[i + 1]) {
-                first++;
+                firstIntersection++;
             }
         }
+        
         return true;
     }
 };
