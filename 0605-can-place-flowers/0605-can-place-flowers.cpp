d29faeb7d4082bf9ad0108263e539159a4270d99
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-02-05 17:39:55 +0200

    Time: 1 ms (17.74%), Space: 24.1 MB (41.3%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -1,45 +1,18 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& f, int n) {
-        if (n == 0)
-            return true;
-        if (f.size() == 1)
-            return f[0] == 0;
-        int maxi = max(evenLoop(f), oddLoop(f));
-        return maxi >= n;
-    }
-
-    int evenLoop(vector<int>& f) {
-        int n = f.size();
         int count = 0;
-        if (f[0] != 1 && f[1] != 1) {
-            count++;
-        }
-        if (f.size() % 2 == 1 && f[n - 1] != 1 && f[n - 2] != 1) {
-            count++;
-        }
-
-        for (int i = 2; i < n - 2; i += 2) {
-            if (f[i - 1] != 1 && f[i + 1] != 1 && f[i] != 1) {
-                count++;
+        for (int i = 0; i < f.size(); i++) {
+            if (f[i] == 0) {
+                bool emptyLeftPlot = ((i == 0) || f[i - 1] == 0);
+                bool emptyRightPlot = ((i == f.size() - 1) || f[i + 1] == 0);
+                if (emptyLeftPlot && emptyRightPlot) {
+                    f[i] = 1;
+                    count++;
+                }
             }
         }
 
-        return count;
-    }
-
-    int oddLoop(vector<int>& f) {
-        int n = f.size();
-        int count = 0;
-        if (f.size() % 2 == 0 && f[n - 1] != 1 && f[n - 2] != 1) {
-            count++;
-        }
-        for (int i = 1; i < n - 1; i += 2) {
-            if (f[i - 1] != 1 && f[i + 1] != 1 && f[i] != 1) {
-                count++;
-            }
-        }
-
-        return count;
+        return count >= n;
     }
 };
