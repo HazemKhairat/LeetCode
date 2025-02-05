fb166c493a8c650b7d84d1ca258adbdee81977f1
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-02-05 11:33:59 +0200

    Time: 3 ms (10.25%), Space: 8.7 MB (9.35%) - LeetHub

--- 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.cpp
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     bool areAlmostEqual(string s1, string s2) {
-        // check anagram
         map<int, int> freq1, freq2;
+        int count = 0;
         for (int i = 0; i < s1.size(); i++) {
             freq1[s1[i]]++;
             freq2[s2[i]]++;
+            if (s1[i] != s2[i]) {
+                count++;
+                if (count > 2) {
+                    return false;
+                }
+            }
         }
+
         for (int i = 0; i < s1.size(); i++) {
             if (freq1[s1[i]] != freq2[s1[i]]) {
                 return false;
             }
         }
 
-        // is it possible to make both strings equal by performing at most one
-        // string swap on exactly one of the strings
-        int count = 0;
-        for (int i = 0; i < s1.size(); i++) {
-            if (s1[i] != s2[i]) {
-                count++;
-            }
-        }
-        return count <= 2;
+        return true;
     }
 };
