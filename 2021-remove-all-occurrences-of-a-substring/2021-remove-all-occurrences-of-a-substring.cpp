37c4f71538800f9d0c53c0aa7164cd73950afa31
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-02-11 13:50:15 +0200

    Time: 12 ms (5.85%), Space: 26.2 MB (5.32%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -1,10 +1,38 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
-        while (s.find(part) != string::npos) {
-            int pos = s.find(part);
-            s.erase(pos, part.size());
+        stack<char> stk;
+        int strLen = s.length();
+        int partLen = part.length();
+
+        for (int i = 0; i < strLen; i++) {
+            stk.push(s[i]);
+            if (stk.size() >= partLen && checkMatch(stk, part, partLen)) {
+                for (int j = 0; j < partLen; j++) {
+                    stk.pop();
+                }
+            }
         }
-        return s;
+
+        string res = "";
+        while (!stk.empty()) {
+            res = stk.top() + res;
+            stk.pop();
+        }
+
+        return res;
+    }
+
+    bool checkMatch(stack<char>& stk, string& part, int partLen) {
+        stack<char> tmp = stk;
+
+        for (int partIdx = partLen - 1; partIdx >= 0; partIdx--) {
+            if (tmp.top() != part[partIdx]) {
+                return false;
+            }
+            tmp.pop();
+        }
+
+        return true;
     }
 };
