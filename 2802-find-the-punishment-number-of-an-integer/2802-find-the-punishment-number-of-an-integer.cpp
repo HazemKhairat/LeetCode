7cbf4e6f282c8981e4bdc499cd2dd765210d6a09
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-02-16 12:05:17 +0200

    Time: 351 ms (18.47%), Space: 11.4 MB (19.21%) - LeetHub

--- 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.cpp
+++ 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.cpp
@@ -3,25 +3,26 @@ public:
     int punishmentNumber(int n) {
         int res = 0;
         for (int i = 1; i <= n; i++) {
-            if (partition(0, 0, i, to_string(i * i))) {
+            if (isPartitioned(0, 0, i, to_string(i * i))) {
                 res += (i * i);
             }
         }
         return res;
     }
 
-    bool partition(int index, int currSum, int target, string squareNumStr) {
-        if (currSum == target && index == squareNumStr.size()) {
-            return true;
+    bool isPartitioned(int index, int currSum, int target, string str) {
+        if (index == str.size()) {
+            return currSum == target;
         }
-        for (int j = index; j < squareNumStr.size(); j++) {
-            if (partition(j + 1,
-                          currSum +
-                              stoi(squareNumStr.substr(index, j - index + 1)),
-                          target, squareNumStr)) {
+
+        for (int j = index; j < str.size(); j++) {
+            string subStr = str.substr(index, j - index + 1);
+            int num = stoi(subStr);
+            if (isPartitioned(j + 1, currSum + num, target, str)) {
                 return true;
             }
         }
+
         return false;
     }
 };
