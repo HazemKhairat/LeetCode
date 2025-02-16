2e875aa6b91153bdfe1527f095bba358e604cc2f
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-02-16 21:31:12 +0200

    Time: 3 ms (19.91%), Space: 9.2 MB (9.96%) - LeetHub

---
+++ 1819-construct-the-lexicographically-largest-valid-sequence/1819-construct-the-lexicographically-largest-valid-sequence.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    vector<int> constructDistancedSequence(int n) {
+        vector<int> res(2 * n - 1, 0);
+        set<int> used;
+        backtrack(0, res, n, used);
+        return res;
+    }
+
+    bool backtrack(int i, vector<int>& res, int n, set<int>& used) {
+        if (i == res.size()) {
+            return true;
+        }
+
+        for (int num = n; num >= 1; num--) {
+            if (used.count(num)) {
+                continue;
+            }
+            if (num > 1 && (i + num >= res.size() or res[i + num])) {
+                continue;
+            }
+
+            used.insert(num);
+            res[i] = num;
+            if (num > 1) {
+                res[i + num] = num;
+            }
+            int j = i + 1;
+            while (j < res.size() && res[j]) {
+                j++;
+            }
+
+            // for (int index = 0; index < res.size(); index++) {
+            //     cout << res[index] << " ";
+            // }
+            // cout << endl;
+            if (backtrack(j, res, n, used)) {
+                return true;
+            }
+
+            used.erase(num);
+            res[i] = 0;
+            if (num > 1) {
+                res[i + num] = 0;
+            }
+        }
+        return false;
+    }
+};
