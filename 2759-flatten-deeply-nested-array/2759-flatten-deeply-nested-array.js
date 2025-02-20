6c6e01be13a4935422bf0b66842394caab36b1ea
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-02-20 19:04:50 +0200

    Time: 80 ms (94.33%), Space: 73.8 MB (65.92%) - LeetHub

--- 2759-flatten-deeply-nested-array/2759-flatten-deeply-nested-array.js
+++ 2759-flatten-deeply-nested-array/2759-flatten-deeply-nested-array.js
@@ -5,22 +5,17 @@
  */
 var flat = function (arr, n) {
     const res = [];
-    for (var item of arr) {
-        if (Array.isArray(item) && n > 0) {
-            res.push(...flat(item, n - 1));
-        }
-        else res.push(item);
-    }
+    recuris(arr, res, 0, n);
     return res;
 };
 
-// var recuris = function (arr, res, depth, n) {
-//     for (const item of arr) {
-//         if (Array.isArray(item) && depth < n) {
-//             recuris(item, res, depth + 1, n);
-//         }
-//         else {
-//             res.push(item);
-//         }
-//     }
-// }
+var recuris = function (arr, res, depth, n) {
+    for (const item of arr) {
+        if (Array.isArray(item) && depth < n) {
+            recuris(item, res, depth + 1, n);
+        }
+        else {
+            res.push(item);
+        }
+    }
+}
