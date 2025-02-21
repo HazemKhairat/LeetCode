76010978809eac2322166fd3c1ce0b64f4bbf5dc
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-02-21 18:57:32 +0200

    Time: 72 ms (94.22%), Space: 65.1 MB (8.41%) - LeetHub

--- 2804-compact-object/2804-compact-object.js
+++ 2804-compact-object/2804-compact-object.js
@@ -5,9 +5,11 @@
 var compactObject = function (obj) {
 
     function dfs(obj) {
-        if (!obj) return false;
-        if (typeof obj != 'object') return obj;
-        if (Array.isArray(obj)) {
+        if (!obj)
+            return false;
+        else if (typeof obj != 'object')
+            return obj;
+        else if (Array.isArray(obj)) {
             const newArr = [];
             for (var i = 0; i < obj.length; i++) {
                 let curr = dfs(obj[i]);
@@ -15,19 +17,20 @@ var compactObject = function (obj) {
                     newArr.push(curr);
                 }
             }
-
             return newArr;
         }
-
-        const newObj = {};
-        for (var key in obj) {
-            let curr = dfs(obj[key]);
-            if (curr) {
-                newObj[key] = curr;
+        else {
+            const newObj = {};
+            for (var key in obj) {
+                let curr = dfs(obj[key]);
+                if (curr) {
+                    newObj[key] = curr;
+                }
             }
+            return newObj;
         }
 
-        return newObj;
+
     }
 
     return dfs(obj);
