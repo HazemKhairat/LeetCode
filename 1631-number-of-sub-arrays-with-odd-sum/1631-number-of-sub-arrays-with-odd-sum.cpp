7d3cc604f7f80de4cd5c0e17f5262e2981d71ab0
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-02-25 19:27:04 +0200

    Time: 4 ms (62.15%), Space: 112.1 MB (55.17%) - LeetHub

--- 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.cpp
+++ 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.cpp
@@ -1,38 +1,21 @@
 class Solution {
 public:
     int numOfSubarrays(vector<int>& arr) {
+        long long pref_sum = 0, odd_cnt = 0, even_cnt = 0, res = 0;
         const int MOD = 1e9 + 7;
-        int n = arr.size();
-        vector<int> dpEven(n), dpOdd(n);
-        for(auto& num : arr){
-            num %= 2;
-        }
-        
-        if(arr[n - 1] == 1){
-            dpOdd[n - 1] = 1;
-        }
-        else{
-            dpEven[n - 1] = 1;
-        }
 
-        for(int idx = n - 2; idx >= 0; idx--){
-            if(arr[idx] == 0){
-                dpEven[idx] = (1 + dpEven[idx + 1]) % MOD;
-                dpOdd[idx] = dpOdd[idx + 1] ;
-            }
-            else{
-                dpOdd[idx] = (1 + dpEven[idx + 1]) % MOD;
-                dpEven[idx] = dpOdd[idx + 1];
+        for (auto num : arr) {
+            pref_sum += num;
+
+            if (pref_sum % 2) {
+                res += (even_cnt + 1);
+                odd_cnt++;
+            } else {
+                res += odd_cnt;
+                even_cnt++;
             }
         }
 
-        int count = 0;
-        for(auto oddCount : dpOdd){
-            count += oddCount;
-            count %= MOD;
-        }
-
-        return count;
+        return (res % MOD);
     }
-
 };
