1652ee1d8b002de514a6b1dbc82dbd8e3eb9659a
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-03-05 04:40:00 +0200

    Time: 0 ms (100%), Space: 8 MB (31.17%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
@@ -1,11 +1,4 @@
 class Solution {
 public:
-    long long coloredCells(int n) {
-        long long curr = 1;
-        for(int i = 2; i <= n; i++){
-            curr = curr + (4 * (i - 1));
-            // cout << curr << endl;
-        }
-        return curr;
-    }
+    long long coloredCells(int n) { return 1 + (long long) n * (n - 1) * 2; }
 };
