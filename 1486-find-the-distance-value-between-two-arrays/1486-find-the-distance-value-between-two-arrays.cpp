60746c6a29dd3f3349367182608afe31148bdc8f
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-03-17 18:04:31 +0200

    Time: 0 ms (100%), Space: 16.7 MB (48.58%) - LeetHub

--- 1486-find-the-distance-value-between-two-arrays/1486-find-the-distance-value-between-two-arrays.cpp
+++ 1486-find-the-distance-value-between-two-arrays/1486-find-the-distance-value-between-two-arrays.cpp
@@ -1,37 +1,29 @@
 class Solution {
 public:
     int findTheDistanceValue(vector<int>& arr1, vector<int>& arr2, int d) {
-        // sort(arr2.begin(), arr2.end());
+        sort(arr2.begin(), arr2.end());
         int res = 0;
         for (int i = 0; i < arr1.size(); i++) {
             int count = 0;
-            // if(search(arr1[i], arr2, d)){
-            //     res++;
-            // }
-            for (int j = 0; j < arr2.size(); j++) {
-                if (abs(arr1[i] - arr2[j]) > d) {
-                    count++;
-                } else {
-                    break;
-                }
-            }
-            if (count == arr2.size()) {
+            if (search(arr1[i], arr2, d)) {
                 res++;
             }
         }
         return res;
     }
 
-    // bool search(int val, vector<int>& arr2, int d) {
-    //     int left = 0, right = arr2.size() - 1;
-    //     while (left <= right) {
-    //         int mid = (left + right) / 2;
-    //         if (abs(val - arr2[mid]) > d) {
-    //             right = mid - 1;
-    //         } else {
-    //             return false;
-    //         }
-    //     }
-    //     return true;
-    // }
+    bool search(int val, vector<int>& arr2, int d) {
+        int left = 0, right = arr2.size() - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (abs(val - arr2[mid]) <= d) {
+                return false;
+            } else if (arr2[mid] < val) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return true;
+    }
 };
