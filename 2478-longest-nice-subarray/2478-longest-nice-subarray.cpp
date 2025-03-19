6a21c45a864920f628b03c2f42b289925e0f0780
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-03-19 22:58:04 +0200

    Time: 3 ms (41.42%), Space: 60.9 MB (89.52%) - LeetHub

--- 2478-longest-nice-subarray/2478-longest-nice-subarray.cpp
+++ 2478-longest-nice-subarray/2478-longest-nice-subarray.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
     int longestNiceSubarray(vector<int>& nums) {
-        int maxLen = 1;
-        for (int start = 0; start < nums.size(); start++) {
-            int currLen = 1;
-            int bitMask = nums[start];
-            for (int end = start + 1; end < nums.size(); end++) {
-                if ((bitMask & nums[end]) == 0) {
-                    bitMask |= nums[end];
-                    currLen++;
-                } else {
-                    break;
-                }
+        int start = 0, end = 0, maxLen = 0, usedBits = 0;
+        for (; end < nums.size(); end++) {
+            while ((usedBits & nums[end]) != 0) {
+                usedBits ^= nums[start];
+                start++;
             }
-            maxLen = max(maxLen, currLen);
+            usedBits |= nums[end];                 
+            maxLen = max(maxLen, end - start + 1); 
         }
         return maxLen;
     }
