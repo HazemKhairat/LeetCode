599ac5ad54296ceb7e1eb2b94efc0a603c6256d2
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-03-23 06:13:57 +0200

    Time: 2057 ms (25.05%), Space: 476.2 MB (15.21%) - LeetHub

--- 3809-properties-graph/3809-properties-graph.cpp
+++ 3809-properties-graph/3809-properties-graph.cpp
@@ -1,39 +1,53 @@
 class Solution {
 public:
-    vector<vector<int>> graph;
-    vector<bool> visited;
+    vector<int> parent, rank;
     int numberOfComponents(vector<vector<int>>& prop, int k) {
         int n = prop.size();
-        visited.resize(n + 5, false);
-        graph.resize(n);
+
+        parent.resize(n);
+        rank.resize(n, 1);
+
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+
         for (int i = 0; i < prop.size(); i++) {
             unordered_set<int> st1(prop[i].begin(), prop[i].end());
+
             for (int j = i + 1; j < prop.size(); j++) {
                 unordered_set<int> st2(prop[j].begin(), prop[j].end());
+
                 if (intersect(st1, st2) >= k) {
-                    graph[i].push_back(j);
-                    graph[j].push_back(i);
+                    Union(i, j);
                 }
             }
         }
 
-        int res = 0;
+        unordered_set<int> res;
         for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                res++;
-                dfs(i);
-            }
+            res.insert(find(i));
         }
-        return res;
+
+        return res.size();
     }
 
-    void dfs(int node) {
-        visited[node] = true;
-        for (auto nighbour : graph[node]) {
-            if (!visited[nighbour]) {
-                dfs(nighbour);
-            }
+    void Union(int n1, int n2) {
+        int p1 = find(n1), p2 = find(n2);
+        if (p1 == p2)
+            return;
+        if (rank[p1] < rank[p2]) {
+            swap(p1, p2);
         }
+        parent[p2] = p1;
+        if (rank[p1] == rank[p2])
+            rank[p1]++;
+    }
+
+    int find(int n) {
+        if (parent[n] != n) {
+            parent[n] = find(parent[n]);
+        }
+        return parent[n];
     }
 
     int intersect(unordered_set<int>& a, unordered_set<int>& b) {
