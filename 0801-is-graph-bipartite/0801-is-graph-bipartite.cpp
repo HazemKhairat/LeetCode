eb73d098031a90dd9812cabfa18fbf17690f1dd9
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-03-29 23:54:59 +0200

    Time: 0 ms (100%), Space: 17.4 MB (40.39%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -1,33 +1,36 @@
 class Solution {
 public:
-    vector<int> vis, colors;
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
-        colors.resize(n);
-        vis.resize(n + 5);
-        bool res = false;
-        for (int i = 0; i < n; i++) {
-            if (!vis[i] && !dfs(graph, 0, i)) {
-                return false;
-            }
-        }
-        return true;
-    }
+        vector<int> color(n); // 1 red, -1 blue
 
-    bool dfs(vector<vector<int>>& graph, int c, int node) {
-        vis[node] = true;
-        colors[node] = c;
-        for (auto nighbour : graph[node]) {
-            if (!vis[nighbour]) {
-                if (!dfs(graph, !c, nighbour)) {
-                    return false;
-                }
-            } else {
-                if (colors[nighbour] == colors[node]) {
-                    return false;
+        queue<int> q;
+
+        for (int i = 0; i < n; i++) {
+            if (!color[i]) {
+                q.push(i);
+                color[i] = 1;
+                while (!q.empty()) {
+                    int size = q.size();
+                    for (int i = 0; i < size; i++) {
+                        int node = q.front();
+                        q.pop();
+
+                        for (auto nighbour : graph[node]) {
+                            if (color[nighbour] == 0) {
+                                q.push(nighbour);
+                                color[nighbour] = color[node] * -1;
+                            } else {
+                                if (color[nighbour] == color[node]) {
+                                    return false;
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
+
         return true;
     }
 };
