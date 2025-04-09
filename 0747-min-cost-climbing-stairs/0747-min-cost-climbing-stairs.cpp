b7c9a592629530c138b4e5a6275a3dafa1ef52a6
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-04-09 17:42:19 +0200

    Time: 0 ms (100%), Space: 17.7 MB (50.67%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
+    vector<int> dp;
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
-        dp = vector<vector<int>>(n, vector<int>(n, -1));
-        return solve(cost, 1, 0);
+        dp = vector<int>(n, -1);
+        return solve(cost, 1);
     }
 
-    int solve(vector<int>& cost, int index, int step) {
+    int solve(vector<int>& cost, int index) {
         if (index >= cost.size()) {
             return 0;
         }
-        if (dp[index][step] != -1) {
-            return dp[index][step];
+        if (dp[index] != -1) {
+            return dp[index];
         }
-        int one = cost[index - 1] + solve(cost, index + 1, step + 1);
-        int two = cost[index] + solve(cost, index + 2, step + 1);
+        int one = cost[index - 1] + solve(cost, index + 1);
+        int two = cost[index] + solve(cost, index + 2);
 
-        return dp[index][step] = min(one, two);
+        return dp[index] = min(one, two);
     }
 };
