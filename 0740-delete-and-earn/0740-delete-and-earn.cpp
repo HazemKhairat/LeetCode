dd83effa7be701fb46d23d378e3098515f97e304
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-04-10 12:09:06 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -1,19 +1,42 @@
 class Solution {
 public:
+    unordered_map<int, int> points;
     int deleteAndEarn(vector<int>& nums) {
-        vector<int> values(10001, 0);
-        for(auto num : nums){
-            values[num] += num;
+        /*  - could we convert it to house ropper problem ? Yes
+            - How ?
+            sorted nums = 2 2 3 3 3 4
+            unique = 2 3 4
+            points 2 3 4
+                   4 9 4
+
+            1 3 5
+        */
+        sort(nums.begin(), nums.end());
+        vector<int> unique;
+        unique.push_back(nums[0]);
+        for (int num : nums) {
+            points[num] += num;
+            if (num != unique.back()) {
+                unique.push_back(num);
+            }
         }
 
-        int take = 0 , skip = 0;
-        for(int i = 0; i < values.size(); i++){
-            int takei = skip + values[i];
-            int skipi = max(skip, take);
-            take = takei;
-            skip = skipi;
+        return solve(unique, 0);
+    }
+
+    int solve(vector<int>& unique, int index) {
+        if (index >= unique.size()) {
+            return 0;
         }
 
+        int take = 0, skip = 0;
+
+        if (index < unique.size() - 1 && unique[index] == unique[index + 1] - 1)
+            take = points[unique[index]] + solve(unique, index + 2);
+        else
+            take = points[unique[index]] + solve(unique, index + 1);
+
+        skip = solve(unique, index + 1);
         return max(take, skip);
     }
 };
