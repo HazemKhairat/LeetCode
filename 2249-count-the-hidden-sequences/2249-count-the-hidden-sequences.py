bea478aa27df27a831489012a62a0795c8ff9276
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-04-21 14:10:26 +0200

    Time: 109 ms (59.6%), Space: 32.4 MB (28.28%) - LeetHub

--- 2249-count-the-hidden-sequences/2249-count-the-hidden-sequences.py
+++ 2249-count-the-hidden-sequences/2249-count-the-hidden-sequences.py
@@ -1,26 +1,11 @@
 class Solution:
     def numberOfArrays(self, diff: List[int], lower: int, upper: int) -> int:
-        # original = 3 -4 5 1 -2 lower = -4, upper = 5
-        # let x = 1
-        # 1 4 0 5 6 4
-        # res = lower - min = -4
-        # add res to each value = -3 0 -4 1 2 0
-        # ans = upper - max + 1 = 5 - 2 + 1 = 4
-
-
-        arr = [0]
-        x , y = 0, 0
+        x = y = curr = 0
         for num in diff:
-            curr = arr[-1] + num
-            arr.append(curr)
+            curr += num
             x = min(x, curr)
             y = max(y, curr)
             if (y - x) > (upper - lower):
                 return 0
-        
-        res = lower - min(arr)
 
-        for i in range(len(arr)):
-            arr[i] += res
-        
-        return upper - max(arr) + 1
+        return (upper - lower) - (y - x) + 1
