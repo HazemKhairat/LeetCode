4e43fafbfbfb9ffaa14b21f099b6923465203ffd
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-04-30 11:42:04 +0300

    Time: 71 ms (88.19%), Space: 18.4 MB (36.52%) - LeetHub

--- 1105-uncrossed-lines/1105-uncrossed-lines.py
+++ 1105-uncrossed-lines/1105-uncrossed-lines.py
@@ -1,22 +1,13 @@
 class Solution:
     def maxUncrossedLines(self, nums1: List[int], nums2: List[int]) -> int:
         n, m = len(nums1), len(nums2)
-        memo = [[-1] * (m + 1) for _ in range(n + 1)]
 
-        def solve(i, j):
-            if i == n or j == m:
-                return 0
+        dp = [[0] * (m + 1) for _ in range(n + 1)]
 
-            if memo[i][j] != -1:
-                return memo[i][j]
+        for i, x in enumerate(nums1):
+            for j, y in enumerate(nums2):
+                dp[i + 1][j + 1] = (
+                    1 + dp[i][j] if x == y else max(dp[i][j + 1], dp[i + 1][j])
+                )
 
-            equal, tryI, tryJ = 0, 0, 0
-            if nums1[i] == nums2[j]:
-                equal = 1 + solve(i + 1, j + 1)
-            tryI = solve(i + 1, j)
-            tryJ = solve(i, j + 1)
-
-            memo[i][j] = max(equal, tryI, tryJ)
-            return memo[i][j]
-
-        return solve(0, 0)
+        return dp[-1][-1]
