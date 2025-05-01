8be5d3d778e9705785ae98c603de71d95614d7af
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-05-01 14:19:09 +0300

    Time: 647 ms (48.2%), Space: 19.4 MB (70.19%) - LeetHub

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.py
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -1,23 +1,16 @@
 class Solution:
     def minInsertions(self, s: str) -> int:
         n = len(s)
-        memo = [[-1] * n for _ in range(n)]
+        dp = [[0] * (n + 1) for _ in range(n + 1)]
+        reversedS = s[::-1]
 
-        def solve(i, j):
-            if i > j:
-                return 0
+        for i in range(1, n + 1):
+            for j in range(1, n + 1):
+                if s[i - 1] == reversedS[j - 1]:
+                    dp[i][j] = 1 + dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
 
-            if memo[i][j] != -1:
-                return memo[i][j]
+        # print(*dp, sep="\n")
 
-            skip = start = end = float("inf")
-            if s[i] == s[j]:
-                skip = solve(i + 1, j - 1)
-
-            start = 1 + solve(i + 1, j)
-            end = 1 + solve(i, j - 1)
-            memo[i][j] = min(start, end, skip)
-
-            return memo[i][j]
-
-        return solve(0, n - 1)
+        return n - dp[n][n]
