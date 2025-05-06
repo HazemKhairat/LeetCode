8c2e861fd92664a91c04dd741275850211aa6ed8
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-05-06 18:52:27 +0300

    Time: 545 ms (88.61%), Space: 0B (100%) - LeetHub

--- 2110-employees-with-missing-information/2110-employees-with-missing-information.sql
+++ 2110-employees-with-missing-information/2110-employees-with-missing-information.sql
@@ -1,12 +1,9 @@
-select employee_id from
-
-(select employee_id from Employees 
-where employee_id not in
-(select e.employee_id from Employees e join Salaries s on e.employee_id = s.employee_id)
+select employee_id from Employees 
+where employee_id not in (select employee_id from Salaries)
 
 union
 
 select employee_id from Salaries where employee_id not in
-(select e.employee_id from Employees e join Salaries s on e.employee_id = s.employee_id)) as subquery
+(select employee_id from Employees)
 
 order by employee_id
