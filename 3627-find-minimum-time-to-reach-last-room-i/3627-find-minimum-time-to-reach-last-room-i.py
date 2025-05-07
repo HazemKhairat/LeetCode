51191c0891ea7db1b7ac0f537c0e2f79f99c7e53
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-05-07 11:02:23 +0300

    Time: 198 ms (14.17%), Space: 18.4 MB (71.12%) - LeetHub

---
+++ 3627-find-minimum-time-to-reach-last-room-i/3627-find-minimum-time-to-reach-last-room-i.py
@@ -0,0 +1,32 @@
+from queue import PriorityQueue
+
+
+class Solution:
+    def minTimeToReach(self, mv: List[List[int]]) -> int:
+        n, m = len(mv), len(mv[0])
+        vis = [[float("inf")] * m for _ in range(n)]
+
+        pq = PriorityQueue()
+        dirs = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        pq.put([0, 0, 0])
+        vis[0][0] = 0
+
+        while not pq.empty():
+            curr = pq.get()
+            t, r, c = curr[0], curr[1], curr[2]
+            if r == n - 1 and c == m - 1:
+                return t
+
+            if t > vis[r][c]:
+                continue
+
+            for dir in dirs:
+                newR, newC = r + dir[0], c + dir[1]
+                if newR < 0 or newC < 0 or newR == n or newC == m:
+                    continue
+                newT = max(t + 1, mv[newR][newC] + 1)
+                if newT < vis[newR][newC]:
+                    vis[newR][newC] = newT
+                    pq.put((newT, newR, newC))
+
+        return -1
