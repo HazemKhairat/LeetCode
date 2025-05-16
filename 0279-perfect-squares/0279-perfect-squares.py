1670b43c76352d0528d7bc8e1b61462db6eec5d3
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-05-16 19:33:25 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.py
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -4,7 +4,7 @@ class Solution:
         for i in range(1,101):
             squares.append(i*i)
         
-        memo = [[-1 for _ in range(len(squares))] for _ in range(n + 1)]
+        memo = [[-1 for _ in range(n + 1)] for _ in range(len(squares))]
 
         def solve(index, n):
             if n == 0:
@@ -12,14 +12,14 @@ class Solution:
             if index == len(squares) or n < 0:
                 return float('inf')
                   
-            if memo[n][index] != -1:
-                return memo[n][index]
+            if memo[index][n] != -1:
+                return memo[index][n]
             take = skip = 0
 
             take = 1 + solve(index, n - squares[index])
             skip = solve(index + 1, n)
 
-            memo[n][index] = min(take, skip)
-            return memo[n][index]
+            memo[index][n] = min(take, skip)
+            return memo[index][n]
         
         return solve(0, n)
