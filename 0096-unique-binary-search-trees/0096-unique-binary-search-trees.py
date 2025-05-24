6536e4b0bbf700dde6e0a67afc804b416880e0d4
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-05-24 09:56:12 +0300

    Time: 0 ms (100%), Space: 17.7 MB (79.13%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
@@ -1,20 +1,23 @@
 class Solution:
     
+
     def numTrees(self, n: int) -> int:
-       
-
-
-        dp = [0] * (n + 1)
+        
+        memo = {}
 
         def solve(n):
-            if n <= 1:
+
+            if n == 0 or n == 1:
                 return 1
-            
-            if dp[n]:
-                return dp[n]
-            res = 0
+
+            if n in memo:
+                return memo[n]
+
+            root = 0
             for i in range(1, n + 1):
-                dp[n] += solve(i - 1) * solve(n - i)
-            return dp[n]
-        
+                root += solve(i - 1) * solve(n - i)
+
+            memo[n] = root
+            return root
+
         return solve(n)
