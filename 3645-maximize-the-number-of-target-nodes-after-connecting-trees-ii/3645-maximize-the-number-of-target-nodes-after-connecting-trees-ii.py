3a327d8922ae3ddc7301d761ad27fc188aeb6cf9
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-05-29 21:39:57 +0300

    Time: 451 ms (70.45%), Space: 88.8 MB (75%) - LeetHub

--- 3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii/3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii.py
+++ 3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii/3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii.py
@@ -1,63 +1,55 @@
 class Solution:
-    def maxTargetNodes(self, edges1: List[List[int]], edges2: List[List[int]]) -> List[int]:
-        n, m = len(edges1), len(edges2)
 
-        tree1 = [[] for _ in range(n + 1)]
-        tree2 = [[] for _ in range(m + 1)]
-
-        for edge in edges1:
-            u,v = edge
-            tree1[u].append(v)
-            tree1[v].append(u)
-
-        for edge in edges2:
-            u,v = edge
-            tree2[u].append(v)
-            tree2[v].append(u)
-
-        # tree1 BFS
+    def BFS(self, tree):
+        n = len(tree)
         q = deque()
         q.append(0)
-        colors = [False] * (n + 1)
+        colors = [False] * (n)
         colors[0] = True
-        vis = [False] * (n + 1)
+        vis = [False] * (n)
         vis[0] = True
 
         while q:
             node = q.popleft()
-            for nighbour in tree1[node]:
+            for nighbour in tree[node]:
                 if not vis[nighbour]:
                     colors[nighbour] = not colors[node]
                     q.append(nighbour)
                     vis[nighbour] = True
+        return colors
+
+    def build(self, edges):
+        n = len(edges)
+        tree = [[] for _ in range(n + 1)]
+        for edge in edges:
+            u, v = edge
+            tree[u].append(v)
+            tree[v].append(u)
         
-        even = sum(colors)
-        odd = len(colors) - even
+        return tree
 
-        # tree2 BFS
+    def maxTargetNodes(
+        self, edges1: List[List[int]], edges2: List[List[int]]
+    ) -> List[int]:
+        n, m = len(edges1), len(edges2)
 
-        q.append(0)
-        colors2 = [False] * (m + 1)
-        colors2[0] = True
-        vis = [False] * (m + 1)
-        vis[0] = True
+        tree1 = self.build(edges1)
+        tree2 = self.build(edges2)
 
-        while q:
-            node = q.popleft()
-            for nighbour in tree2[node]:
-                if not vis[nighbour]:
-                    colors2[nighbour] = not colors2[node]
-                    q.append(nighbour)
-                    vis[nighbour] = True
-                
+        colors1 = self.BFS(tree1)
+        colors2 = self.BFS(tree2)
+
+        even = sum(colors1)
+        odd = n - even + 1
+
         temp = sum(colors2)
         maxi = max(temp, m + 1 - temp)
 
         res = [0] * (n + 1)
         for i in range(n + 1):
-            if colors[i]:
+            if colors1[i]:
                 res[i] = even + maxi
             else:
                 res[i] = odd + maxi
-            
+
         return res
