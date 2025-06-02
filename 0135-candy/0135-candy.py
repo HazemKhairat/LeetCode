0987cff17d855f37caa2741dc02c7bf2aa9f65a3
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-06-02 15:51:58 +0300

    Time: 18 ms (45.29%), Space: 19.9 MB (36.16%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -1,31 +1,14 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
         n = len(ratings)
-        arr = [[float('inf')] * 3 for _ in range(n)]
-        # print(arr)
-
-        for i in range(n):
-            arr[i][1] = (ratings[i], i)
-            if i > 0:
-                arr[i][0] = (ratings[i - 1], i - 1)
-            if i < n - 1:
-                arr[i][2] = (ratings[i + 1], i + 1)
-        arr = sorted(arr, key=lambda x: x[1])
-
-        candies = Counter()
-        for i in range(n):
-            item = arr[i][1]
-            candies[item] = 1
-        print(arr)
+        candies = [1] * n
 
         for i in range(1, n):
-            curr, last, next = arr[i][1], arr[i][0], arr[i][2]
-            if  last != float('inf') and curr[0] > last[0] and candies[curr] <= candies[last]:
-                candies[curr] = candies[last] + 1
-            if next != float('inf') and curr[0] > next[0] and candies[curr] <= candies[next]:
-                candies[curr] = candies[next] + 1
+            if ratings[i] > ratings[i - 1]:
+                candies[i] = candies[i - 1] + 1
 
-        res = 0 
-        for [c, num] in candies.items():
-            res += num
-        return res
+        for i in range(n - 2, -1, -1):
+            if ratings[i] > ratings[i + 1]:
+                candies[i] = max(candies[i], candies[i + 1] + 1)
+
+        return sum(candies)
