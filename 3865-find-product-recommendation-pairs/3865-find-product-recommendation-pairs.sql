17cc432e5d3109618ac7aa84d35d79014321955d
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-06-30 15:03:19 +0300

    Time: 576 ms (79.77%), Space: 0B (100%) - LeetHub

--- 3865-find-product-recommendation-pairs/3865-find-product-recommendation-pairs.sql
+++ 3865-find-product-recommendation-pairs/3865-find-product-recommendation-pairs.sql
@@ -1,14 +1,14 @@
-select
-p1.product_id as product1_id, 
-p2.product_id as product2_id,
-pi1.category as product1_category, 
-pi2.category as product2_category,
-count(p1.user_id) as customer_count 
-from 
+select 
+p1.product_id as product1_id
+,p2.product_id as product2_id
+,pi1.category as product1_category 
+,pi2.category as product2_category 
+,count(p1.user_id) as customer_count
+from
 ProductPurchases p1 join ProductPurchases p2
-on p1.user_id = p2.user_id and p1.product_id < p2.product_id
+on p1.user_id = p2.user_id and p1.product_id < p2.product_id 
 join ProductInfo pi1 on pi1.product_id = p1.product_id
 join ProductInfo pi2 on pi2.product_id = p2.product_id
-group by product1_id, product2_id
-having count(p1.user_id) >= 3
-order by customer_count desc, product1_id, product2_id 
+group by product1_id , product2_id
+having customer_count >= 3
+order by customer_count desc, product1_id, product2_id
