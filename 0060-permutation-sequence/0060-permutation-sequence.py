a1006abf2427ac2536969caa121322b1c802b7a7
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-07-07 18:46:38 +0300

    Time: 8307 ms (5.03%), Space: 17.8 MB (81.44%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.py
+++ 0060-permutation-sequence/0060-permutation-sequence.py
@@ -1,24 +1,22 @@
 class Solution:
     def getPermutation(self, n: int, k: int) -> str:
-        permutations = set()
+        res = ""
 
         def solve(st):
-            nonlocal k
+            nonlocal k, res
+
             if len(st) >= n:
                 k -= 1
                 if k == 0:
-                    return st
-                return ""
+                    res = st
+                return
             
-            res = ""
             for i in range(1, n + 1):
                 ch = str(i)
                 if ch in st:
                     continue
-                res = solve(st + ch)
-                if res != "":
-                    return res
-            return res
-        
-        
-        return solve("")
+                solve(st + ch)
+                if res:
+                    return        
+        solve("")
+        return res
