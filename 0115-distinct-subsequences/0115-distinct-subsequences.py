0c5ea95ecc80ed1bcb50e9fd4af43bebe0c4d9e3
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-07-09 14:59:10 +0300

    Time: 493 ms (38.85%), Space: 41.5 MB (73.51%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -1,18 +1,21 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
         n, m = len(s), len(t)
+        memo = [[-1] * n for _ in range(n)]
 
-        @cache
         def backtrack(idxS, idxT):
             if idxT == m: return 1
             if idxS == n: return 0
+
+            if memo[idxS][idxT] != -1:
+                return memo[idxS][idxT]
             
             take = skip = 0
             if s[idxS] == t[idxT]:
                 take += backtrack(idxS + 1, idxT + 1)
             
             skip += backtrack(idxS + 1, idxT)
-
-            return take + skip
+            memo[idxS][idxT] = take + skip
+            return memo[idxS][idxT]
 
         return backtrack(0, 0)
