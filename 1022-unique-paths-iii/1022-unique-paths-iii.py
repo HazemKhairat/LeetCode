d39bd94dd3d6318211eecb9526e4d05aebcc9ce9
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-07-14 19:19:06 +0300

    Time: 9 ms (78.98%), Space: 18 MB (32.83%) - LeetHub

--- 1022-unique-paths-iii/1022-unique-paths-iii.py
+++ 1022-unique-paths-iii/1022-unique-paths-iii.py
@@ -1,41 +1,33 @@
 class Solution:
     def uniquePathsIII(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
-        visit = [[0] * n for i in range(m)]
-                
-        def allVisited(visit):
-            
-            for i in range(len(visit)):
-                for j in range(len(visit[0])):
-                    if grid[i][j] == -1 or grid[i][j] == 2:
-                        visit[i][j] = 1
-                    if visit[i][j] == 0:
-                        return False
-            print(visit)
-            
-            return True
-
-        def dfs(i, j):
-            if i < 0 or i == m or j < 0 or j == n or grid[i][j] == -1 or visit[i][j]:
-                return 0
-            if grid[i][j] == 2 and allVisited(visit):
-                visit[i][j] = 0 # backtrack from making 2 cell visited
-                return 1
-            
-            visit[i][j] = 1
-            left = dfs(i, j -1)
-            right = dfs(i, j + 1)
-            up = dfs(i - 1, j)
-            down = dfs(i + 1, j)
-            visit[i][j] = 0
-
-            return left + right + up + down
         
-        res = 0
+        total = 1
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == 1:
-                    res = dfs(i, j)
+                if grid[i][j] == 0:
+                    total += 1
+                elif grid[i][j] == 1:
+                    start_x, start_y = i, j
+        
+
+        def dfs(i, j):
+            nonlocal total
+            if i < 0 or i == m or j < 0 or j == n or grid[i][j] < 0:
+                return 0
+
+            if grid[i][j] == 2:
+                return 1 if total == 0 else 0
+            
+            grid[i][j] = -2
+            total -= 1
+            path = ( dfs(i, j -1) + dfs(i, j + 1) + dfs(i - 1, j) + dfs(i + 1, j) )
+            grid[i][j] = 0
+            total += 1
+
+            return path
+        
+       
                 
-        return res
+        return dfs(start_x, start_y)
             
