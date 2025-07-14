881cdb33635ccf1247e5ce1ee0331cdf514ab386
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-07-14 18:54:37 +0300

    Time: 23 ms (25.13%), Space: 17.9 MB (51.09%) - LeetHub

---
+++ 1022-unique-paths-iii/1022-unique-paths-iii.py
@@ -0,0 +1,40 @@
+class Solution:
+    def uniquePathsIII(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        visit = [[0] * n for i in range(m)]
+                
+        def allVisited(visit):
+            
+            for i in range(len(visit)):
+                for j in range(len(visit[0])):
+                    if grid[i][j] == -1 or grid[i][j] == 2:
+                        continue
+                    if visit[i][j] == 0:
+                        return False
+            print(visit)
+            
+            return True
+
+        def dfs(i, j):
+            if i < 0 or i == m or j < 0 or j == n or grid[i][j] == -1 or visit[i][j]:
+                return 0
+            if grid[i][j] == 2 and allVisited(visit):
+                return 1
+            
+            visit[i][j] = 1
+            left = dfs(i, j -1)
+            right = dfs(i, j + 1)
+            up = dfs(i - 1, j)
+            down = dfs(i + 1, j)
+            visit[i][j] = 0
+
+            return left + right + up + down
+        
+        res = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    res = dfs(i, j)
+                
+        return res
+            
