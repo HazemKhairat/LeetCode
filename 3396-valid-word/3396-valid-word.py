a36b497094eb38deef2b0825b40dbc0e426bd4e1
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-07-15 09:51:29 +0300

    Time: 0 ms (100%), Space: 17.9 MB (34.46%) - LeetHub

--- 3396-valid-word/3396-valid-word.py
+++ 3396-valid-word/3396-valid-word.py
@@ -2,26 +2,18 @@ class Solution:
     def isValid(self, word: str) -> bool:
         if len(word) < 3:
             return False
-
-        vowel = ['a', 'e', 'i', 'o','u']
-        digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-        lower = [chr(i) for i in range(97, 123)]
-        upper = [chr(i) for i in range(65, 91)]
-
-        f = s = l = False
-        t = True
-        for ch in word:
-            if ch.lower() in vowel:
-                f = True
-            elif ch in lower or ch in upper or ch in digits:
-                if ch.lower() not in vowel and ch not in digits:
-                    l = True
-                s = True
-            else:
-                t = False
         
-        return f and s and t and l
-            
-            
+        has_vowel = False
+        has_consonant  = False
 
-      
+        for ch in word:
+            if ch.isalpha():
+                if ch.lower() in "aeiou":
+                    has_vowel = True
+                else:
+                    has_consonant  = True
+            elif not ch.isdigit():
+                return False
+            
+        return has_vowel and has_consonant 
+
