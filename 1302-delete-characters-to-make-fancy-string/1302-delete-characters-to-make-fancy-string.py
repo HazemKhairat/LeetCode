1916c314e7e7cc3892378d4ab8ac5e45e34750a0
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-07-21 11:58:47 +0300

    Time: 286 ms (85.49%), Space: 19.3 MB (90.85%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.py
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.py
@@ -1,28 +1,19 @@
 class Solution:
     def makeFancyString(self, s: str) -> str:
-        if len(s) <= 2:
-            return s
-
-        res = ""
-        cnt = Counter()
-        cnt[s[0]] += 1
-        for i in range(1, len(s)):
-            if s[i] not in cnt:
-                if cnt[s[i - 1]] >= 2:
-                    res += s[i - 1] + s[i - 1]
-                    cnt.pop(s[i - 1])
-                else:
-                    res += s[i - 1]
-                    cnt.pop(s[i - 1])
-            cnt[s[i]] += 1
-        first_key = next(iter(cnt))
-
-        if cnt[first_key] >= 2:
-            res += first_key + first_key
-        else:
-            res += first_key
-
-        return res
-
- 
         
+        prev = s[0] # a
+        freq = 1
+        ans = s[0] # aabaa
+
+        for i in range(1, len(s)):
+            if s[i] == prev: # a , a
+                freq += 1 # 4
+            else:
+                prev = s[i]
+                freq = 1
+            
+            if freq < 3:
+                ans += s[i]
+            
+        return ans
+
