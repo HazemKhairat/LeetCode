1012bb1c68766f419de4bdbc752905d7d38dcd0d
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-07-28 00:19:17 +0300

    Time: 472 ms (100%), Space: 25.9 MB (0%) - LeetHub

--- 3948-maximum-number-of-subsequences-after-one-inserting/3948-maximum-number-of-subsequences-after-one-inserting.py
+++ 3948-maximum-number-of-subsequences-after-one-inserting/3948-maximum-number-of-subsequences-after-one-inserting.py
@@ -1,31 +1,30 @@
 class Solution:
     def numOfSubsequences(self, s: str) -> int:
         n = len(s)
-        prefix = [0] * (n + 1)  # `L` count (1-based)
-        suffix = [0] * (n + 1)  # `T` count (0-based)
+        prefL = [0] * (n + 1)
+        suffT = [0] * (n + 1)
 
-        for i in range(n):
-            if s[i] == 'L':
-                prefix[i + 1] = 1
-            prefix[i + 1] += prefix[i]
-
+        for i in range(1, n):
+            if s[i - 1] == 'L':
+                prefL[i] = 1
+            prefL[i] += prefL[i - 1]
+        
         for i in range(n - 1, -1, -1):
             if s[i] == 'T':
-                suffix[i] = 1
-            suffix[i] += suffix[i + 1]
+                suffT[i] = 1
+            suffT[i] += suffT[i + 1]
+            
 
-        resWithC = 0
-        bestPosForC = 0
-        resWithL = 0
-        resWithT = 0
-
+        l, c, t, bestPosForC = 0, 0, 0, 0
         for i in range(n):
             if s[i] == 'C':
-                resWithC += prefix[i] * suffix[i]
-                resWithL += (prefix[i] + 1) * suffix[i]
-                resWithT += prefix[i] * (suffix[i] + 1)
+                c += prefL[i] * suffT[i]
+                l += (prefL[i] + 1) * suffT[i]
+                t += prefL[i] * (suffT[i] + 1)
             else:
-                bestPosForC = max(bestPosForC, prefix[i] * suffix[i])
+                bestPosForC = max(bestPosForC, prefL[i] * suffT[i])
 
-        resWithC += bestPosForC
-        return max(resWithL, resWithT, resWithC)
+        c += bestPosForC
+
+        return max(l, c, t)
+
