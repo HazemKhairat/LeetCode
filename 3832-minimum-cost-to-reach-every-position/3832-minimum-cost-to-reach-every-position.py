2644b80a4478a452cdc3b8efb57c910ad46c2e56
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-07-30 10:22:41 +0300

    Time: 0 ms (100%), Space: 17.7 MB (62.25%) - LeetHub

---
+++ 3832-minimum-cost-to-reach-every-position/3832-minimum-cost-to-reach-every-position.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minCosts(self, cost: List[int]) -> List[int]:
+        for i in range(len(cost) - 1):
+            if cost[i] < cost[i + 1]:
+                cost[i + 1] = cost[i]
+
+        return cost
+        
