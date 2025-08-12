3b299f4d71965fb1f05392cf2fe7197e4c9bab22
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-08-12 11:16:16 +0300

    Time: 1878 ms (30.23%), Space: 687.3 MB (5.81%) - LeetHub

--- 2882-ways-to-express-an-integer-as-sum-of-powers/2882-ways-to-express-an-integer-as-sum-of-powers.py
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/2882-ways-to-express-an-integer-as-sum-of-powers.py
@@ -1,12 +1,7 @@
 class Solution:
     def numberOfWays(self, n: int, x: int) -> int:
         MOD = 10**9 + 7
-        powers = []
-        l = 1
-        while l**x <= n:
-            powers.append(l**x)
-            l += 1
-        
+    
         @cache
         def solve(curr, n):
             power = curr ** x
