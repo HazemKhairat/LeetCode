4621a12092e8f06884594fb5ac32db164e5f0163
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-08-18 11:22:37 +0300

    Time: 316 ms (0%), Space: 34.1 MB (0%) - LeetHub

--- 3980-best-time-to-buy-and-sell-stock-using-strategy/3980-best-time-to-buy-and-sell-stock-using-strategy.py
+++ 3980-best-time-to-buy-and-sell-stock-using-strategy/3980-best-time-to-buy-and-sell-stock-using-strategy.py
@@ -1,22 +1,19 @@
 class Solution:
     def maxProfit(self, p: List[int], s: List[int], k: int) -> int:
         n = len(p)
+        orig = sum(p[i] * s[i] for i in range(n))
 
-        original = sum(p[i] * s[i] for i in range(n))
-
-        pref_s = [0] * (n + 1)
         pref_p = [0] * (n + 1)
+        pref_s = [0] * (n + 1)
 
         for i in range(n):
-            pref_s[i + 1] = pref_s[i] + s[i] * p[i]
             pref_p[i + 1] = pref_p[i] + p[i]
-
-
-        maxi = -inf
-
+            pref_s[i + 1] = pref_s[i] + (p[i] * s[i])
+        
+        tmp = orig
         for l in range(n - k + 1):
-            prev = pref_s[l + k] - pref_s[l]
-            curr = pref_p[l + k] - pref_p[l + ( k // 2)]
-            maxi = max(maxi, curr - prev)
-
-        return original + max(0, maxi)
+            old = pref_s[l + k] - pref_s[l]
+            new = pref_p[l + k] - pref_p[l + k // 2]
+            orig = max(orig, tmp + (new - old))
+        
+        return orig
