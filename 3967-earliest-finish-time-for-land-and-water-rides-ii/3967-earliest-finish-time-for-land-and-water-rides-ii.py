de6a76bc54a5bd237a31256570802beae3a3d552
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-08-23 20:21:02 +0300

    Time: 131 ms (95.94%), Space: 32.1 MB (50.21%) - LeetHub

--- 3967-earliest-finish-time-for-land-and-water-rides-ii/3967-earliest-finish-time-for-land-and-water-rides-ii.py
+++ 3967-earliest-finish-time-for-land-and-water-rides-ii/3967-earliest-finish-time-for-land-and-water-rides-ii.py
@@ -1,14 +1,17 @@
 class Solution:
     def earliestFinishTime(self, ls: List[int], ld: List[int], ws: List[int], wd: List[int]) -> int:
-        
-        lands = [start + dur for start, dur in zip(ls, ld)]
-        
-        waters = [start + dur for start, dur in zip(ws, wd)]
-        
-        minLand = min(lands)
-        option1 = min( max(minLand, ws[j]) + wd[j] for j in range(len(ws)))
-        
-        minWater = min(waters)
-        option2 = min( max(minWater, ls[i]) + ld[i] for i in range(len(ls)))
-        
+
+        landSum = [ls[i] + ld[i] for i in range(len(ls))]
+        waterSum = [ws[i] + wd[i] for i in range(len(ws))]
+
+        minLandSum = min(landSum)
+        minWaterSum = min(waterSum)
+
+        option1 = option2 = inf
+        for i in range(len(ws)):
+            option1 = min(option1, minLandSum + wd[i] if ws[i] <= minLandSum else waterSum[i])
+
+        for i in range(len(ls)):
+            option2 = min(option2, minWaterSum + ld[i] if ls[i] <= minWaterSum else landSum[i])
+
         return min(option1, option2)
