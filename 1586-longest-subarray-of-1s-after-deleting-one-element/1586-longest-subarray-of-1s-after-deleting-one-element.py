26a410a4077bf2ffe450904bd1bbd6499cc64629
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-08-24 10:11:54 +0300

    Time: 43 ms (81.37%), Space: 22.9 MB (6.2%) - LeetHub

--- 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.py
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.py
@@ -1,32 +1,18 @@
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
+        nums = [0] + nums + [0]
         n = len(nums)
-        if n == 1:
-            return 0
-        pref = [0] * (n)
-        suff = [0] * (n)
-
-        pref[0] = 1 if nums[0] == 1 else 0
-        for i in range(1, n):
-            if nums[i] == 0:
-                pref[i] = 0
-            else:
-                pref[i] = 1 + pref[i - 1]
-
-        maxi = max(pref) - 1
-        suff[n - 1] = 1 if nums[-1] == 1 else 0
-        for i in range(n - 2, -1, -1):
-            if nums[i] == 0:
-                suff[i] = 0
-            else:
-                suff[i] = 1 + suff[i + 1]
-
+        dp = []
+        maxi = 0
+        tmp = 0
         for i in range(n):
-            if nums[i] == 0 and i == 0:
-                maxi = max(maxi, suff[i + 1])
-            elif nums[i] == 0 and i == n - 1:
-                maxi = max(maxi, pref[i - 1])
-            elif nums[i] == 0:
-                maxi = max(maxi, pref[i - 1] + suff[i + 1])
+            if nums[i] == 0:
+                dp.append(tmp)
+                tmp = 0
+            else:
+                tmp += 1
+        
+        for i in range(1, len(dp)):
+            maxi = max(maxi, dp[i] + dp[i - 1])
 
-        return maxi
+        return maxi - 1 if maxi == n - 2 else maxi
