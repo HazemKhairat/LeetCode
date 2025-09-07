746e1ab915ec57640da229ade3e6b53454fbd35d
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-09-07 12:42:15 +0300

    Time: 2553 ms (46.97%), Space: 150.4 MB (24.59%) - LeetHub

--- 3628-find-minimum-time-to-reach-last-room-ii/3628-find-minimum-time-to-reach-last-room-ii.py
+++ 3628-find-minimum-time-to-reach-last-room-ii/3628-find-minimum-time-to-reach-last-room-ii.py
@@ -1,39 +1,33 @@
-from queue import PriorityQueue
-
-
 class Solution:
-    def minTimeToReach(self, mv: List[List[int]]) -> int:
-        n, m = len(mv), len(mv[0])
-        vis = [[float("inf")] * m for _ in range(n)]
-        dirs = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-        pq = PriorityQueue()
-
-        pq.put((0, 0, 0, 0))  # time, row, col, next
-
-        while not pq.empty():
-            t, r, c, nextMove = pq.get()
-
-            if t >= vis[r][c]:
-                continue
-            vis[r][c] = t
-
+    def minTimeToReach(self, mt: List[List[int]]) -> int:
+        n, m = len(mt), len(mt[0])
+        dirs = [[-1, 0], [1, 0], [0, 1], [0, -1]]
+        pq = [] # time, r, c, steps
+        heapq.heappush(pq, [0, 0, 0, 0])
+        vis = set()
+        vis.add((0,0))
+        
+        def notValid(r, c):
+            return r < 0 or c < 0 or r == n or c == m
+            
+        while pq:
+            t, r, c, s = heapq.heappop(pq)
             if r == n - 1 and c == m - 1:
                 return t
+            
+            alt = 1 if s % 2 == 0 else 2
+            for dir in dirs:
+                nr = r + dir[0]
+                nc = c + dir[1]
+                if notValid(nr, nc): continue
+                if (nr, nc) in vis: continue
+                vis.add((nr, nc))
+                newT = max(t, mt[nr][nc]) + alt
+                heapq.heappush(pq, [newT, nr, nc, s + 1])
+        
+        return 0
+            
 
-            for lr, lc in dirs:
-                nr, nc = lr + r, lc + c
 
-                if nr < 0 or nr == n or nc < 0 or nc == m:
-                    continue
 
-                flag = 2 if nextMove == 1 else 1
 
-                newT = max(t + flag, mv[nr][nc] + flag)
-
-                if r == 0 and c == 0 and mv[nr][nc] <= t:
-                    newT = flag + mv[nr][nc]
-
-                if newT < vis[nr][nc]:
-                    pq.put((newT, nr, nc, flag))
-
-        return -1
