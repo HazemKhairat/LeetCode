f5548722362594f8fc72d018ab75f62b7e4be261
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-09-27 11:43:35 +0300

    Time: 0 ms (100%), Space: 17.8 MB (51.9%) - LeetHub

---
+++ 3214-maximize-area-of-square-hole-in-grid/3214-maximize-area-of-square-hole-in-grid.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maximizeSquareHoleArea(self, n: int, m: int, h: List[int], v: List[int]) -> int:
+        h.sort()
+        v.sort()
+
+        def solve(arr):
+            i = 0
+            res = 1
+            while i < len(arr) - 1:
+                l = 1
+                while i < len(arr) - 1 and arr[i + 1] - arr[i] == 1:
+                    l += 1
+                    i += 1
+                res = max(res, l)
+                i += 1
+
+            return res
+
+
+
+        
+        return (min(solve(h), solve(v)) + 1) ** 2
