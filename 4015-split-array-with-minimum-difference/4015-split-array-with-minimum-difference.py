e76e491adbe31e3e32c025aa348d57e99e1be77c
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-09-28 19:54:05 +0300

    Time: 180 ms (60.57%), Space: 30.5 MB (90.24%) - LeetHub

---
+++ 4015-split-array-with-minimum-difference/4015-split-array-with-minimum-difference.py
@@ -0,0 +1,26 @@
+class Solution:
+    def splitArray(self, nums: List[int]) -> int:
+        n = len(nums)
+        total = sum(nums)
+        left = [True] * n
+        right = [True] * n
+
+        for i in range(1, n):
+            left[i] = left[i - 1] and nums[i] > nums[i - 1]
+        for i in range(n - 2, -1, -1):
+            right[i] = right[i + 1] and nums[i] > nums[i + 1]
+        
+        # print(left)
+        # print(right)
+        
+        pref = 0
+        ans = inf
+        for i in range(n - 1):
+            pref += nums[i]
+            if left[i] and right[i + 1]:
+                ans = min(ans, abs(pref - (total - pref)))
+            
+        return ans if ans != inf else -1
+
+
+            
