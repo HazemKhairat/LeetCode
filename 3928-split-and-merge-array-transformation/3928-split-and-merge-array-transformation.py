15471167e5c0639dc06e77e73333fb235a612927
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-09-29 14:31:10 +0300

    Time: 87 ms (90.13%), Space: 18.1 MB (25.28%) - LeetHub

--- 3928-split-and-merge-array-transformation/3928-split-and-merge-array-transformation.py
+++ 3928-split-and-merge-array-transformation/3928-split-and-merge-array-transformation.py
@@ -1,30 +1,21 @@
 class Solution:
     def minSplitMerge(self, nums1: List[int], nums2: List[int]) -> int:
-
-        if nums1 == nums2:
-            return 0
-
-        n = len(nums1)
+        if nums1 == nums2: return 0
         q = deque()
-        vis = set()
-        q.append((nums1, 0))  # curr_array , steps
-        vis.add(tuple(nums1))
-
+        q.append((nums1, 0))  # arr, operation_num
+        vis = set(tuple(nums1))
+        n = len(nums1)
         while q:
-            arr, steps = q.popleft()
-
+            arr, op = q.popleft()
             for l in range(n):
                 for r in range(l, n):
-                    sub = arr[l : r + 1]
+                    rmv = arr[l : r + 1]
                     tmp = arr[:l] + arr[r + 1 :]
-
-                    for i in range(len(tmp)):
-                        new_arr = tmp[:i] + sub + tmp[i:]
+                    for k in range(len(tmp)):
+                        new_arr = tmp[:k] + rmv + tmp[k:]
                         if new_arr == nums2:
-                            return steps + 1
-
+                            return op + 1
                         if tuple(new_arr) not in vis:
                             vis.add(tuple(new_arr))
-                            q.append((new_arr, steps + 1))
-
+                            q.append((new_arr, op + 1))
         return 0
