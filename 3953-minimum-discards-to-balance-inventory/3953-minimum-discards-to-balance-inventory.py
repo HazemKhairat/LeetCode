133c3b29786cdcd14293725509b0d7ccf2c2e8e5
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-10-03 11:37:51 +0300

    Time: 84 ms (98.28%), Space: 31.5 MB (99.65%) - LeetHub

--- 3953-minimum-discards-to-balance-inventory/3953-minimum-discards-to-balance-inventory.py
+++ 3953-minimum-discards-to-balance-inventory/3953-minimum-discards-to-balance-inventory.py
@@ -1,17 +1,23 @@
 class Solution:
     def minArrivalsToDiscard(self, arr: List[int], w: int, m: int) -> int:
-        cnt = Counter()
-        q = deque()
+        n = len(arr)
+        maxi = max(arr)
+        keep = [False] * n
+        cnt = [0] * (maxi + 1)
         ans = 0
 
-        for i in range(len(arr)):
-            if q and q[0] <= i - w:
-                cnt[arr[q.popleft()]] -= 1
-
-            if cnt[arr[i]] < m:
-                cnt[arr[i]] += 1
-                q.append(i)
+        for i in range(n):
+            j = i - w
+            if j >= 0 and keep[j]:
+                prev = arr[j]
+                cnt[prev] -= 1
+            
+            curr = arr[i]
+            if cnt[curr] < m:
+                cnt[curr] += 1
+                keep[i] = True
             else:
                 ans += 1
+                keep[i] = False
 
         return ans
