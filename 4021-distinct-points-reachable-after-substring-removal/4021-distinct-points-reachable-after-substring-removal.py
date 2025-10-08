731165e67e87c86f3ecfcc2072ae10ae5ea31058
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-10-08 18:36:27 +0300

    Time: 349 ms (68.38%), Space: 37.1 MB (29.48%) - LeetHub

--- 4021-distinct-points-reachable-after-substring-removal/4021-distinct-points-reachable-after-substring-removal.py
+++ 4021-distinct-points-reachable-after-substring-removal/4021-distinct-points-reachable-after-substring-removal.py
@@ -1,24 +1,24 @@
 class Solution:
     def distinctPoints(self, s: str, k: int) -> int:
+
         pref = [(0, 0)]
-        x, y = 0, 0
+        x = y = 0
         for ch in s:
-            if ch == "L":
-                x -= 1
-            elif ch == "R":
-                x += 1
-            elif ch == "U":
+            if ch == "U":
                 y += 1
             elif ch == "D":
                 y -= 1
+            elif ch == "L":
+                x -= 1
+            elif ch == "R":
+                x += 1
             pref.append((x, y))
 
-        n = len(pref)
-        total = pref[-1]
-        dist_points = set()
-        for i in range(n - k):
-            X = pref[i][0] + (total[0] - pref[i + k][0])
-            Y = pref[i][1] + (total[1] - pref[i + k][1])
-            dist_points.add((X, Y))
+        ans = set()
+        lx, ly = pref[-1][0], pref[-1][1]
+        for i in range(len(pref) - k):
+            nx = lx - pref[i + k][0] + pref[i][0]
+            ny = ly - pref[i + k][1] + pref[i][1]
+            ans.add((nx, ny))
 
-        return len(dist_points)
+        return len(ans)
