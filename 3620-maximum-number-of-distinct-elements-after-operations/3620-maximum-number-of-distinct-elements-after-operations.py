10e2f5fac9f3abbd64f6690a550df1f10ce3142c
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-10-18 18:03:11 +0300

    Time: 773 ms (37.87%), Space: 36.9 MB (31.36%) - LeetHub

--- 3620-maximum-number-of-distinct-elements-after-operations/3620-maximum-number-of-distinct-elements-after-operations.py
+++ 3620-maximum-number-of-distinct-elements-after-operations/3620-maximum-number-of-distinct-elements-after-operations.py
@@ -1,6 +1,7 @@
 class Solution:
     def maxDistinctElements(self, nums: List[int], k: int) -> int:
         nums.sort()
+        n = len(nums)
         prev = -inf
 
         for i, num in enumerate(nums):
