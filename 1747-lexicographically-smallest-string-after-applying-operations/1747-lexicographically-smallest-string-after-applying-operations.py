672c5806f6d4cf56b3dc0c611e86f45d65bd7fb7
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-10-19 10:43:17 +0300

    Time: 369 ms (89.93%), Space: 26.4 MB (38.13%) - LeetHub

--- 1747-lexicographically-smallest-string-after-applying-operations/1747-lexicographically-smallest-string-after-applying-operations.py
+++ 1747-lexicographically-smallest-string-after-applying-operations/1747-lexicographically-smallest-string-after-applying-operations.py
@@ -1,38 +1,25 @@
 class Solution:
     def findLexSmallestString(self, s: str, a: int, b: int) -> str:
-        tmp = []
-        for ch in s:
-            tmp.append(int(ch))
-        ans = tmp.copy()
+        n = len(s)
+        ans = [int(ch) for ch in s]
         q = deque()
         q.append(ans)
-        n = len(s)
         vis = set()
 
         while q:
             root = q.popleft()
             ans = min(ans, root)
-            newS = root.copy()
-            for i in range(n, 2):
-                newS[i] = (root[i] + a) % 10
+            s1, s2 = root.copy(), (root[b:] + root[:b])
 
-            if tuple(newS) not in vis:
-                q.append(newS)
-                vis.add(tuple(newS))
-            newS = root.copy()
             for i in range(1, n, 2):
-                newS[i] = (root[i] + a) % 10
+                s1[i] = (root[i] + a) % 10
 
-            if tuple(newS) not in vis:
-                q.append(newS)
-                vis.add(tuple(newS))
+            s1, s2 = tuple(s1), tuple(s2)
 
-            newS = root[b:] + root[:b]
-            if tuple(newS) not in vis:
-                q.append(newS)
-                vis.add(tuple(newS))
+            for st in (s1, s2):
+                if st not in vis:
+                    q.append(list(st))
+                    vis.add(st)
 
-        for i in range(len(ans)):
-            ans[i] = str(ans[i])
-
+        ans = [str(ch) for ch in ans]
         return "".join(ans)
