8af42351bf45cec1ae5fc31c87783c0cb13b217d
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-10-23 08:41:08 +0300

    Time: 440 ms (75.15%), Space: 30 MB (88.39%) - LeetHub

---
+++ 3622-maximum-frequency-of-an-element-after-performing-operations-i/3622-maximum-frequency-of-an-element-after-performing-operations-i.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxFrequency(self, nums: List[int], k: int, ops: int) -> int:
+        maxi = max(nums) + k + 1
+        cnt = [0] * (maxi + 1)
+        
+        for num in nums:
+            cnt[num] += 1
+        for i in range(1, maxi + 1):
+            cnt[i] += cnt[i - 1]
+
+        res = 0
+        for i in range(maxi):
+            left = max(0, i - k)
+            right = min(maxi, i + k)
+            total = cnt[right] - (cnt[left - 1] if left else 0)
+            freq = cnt[i] - (cnt[i - 1] if i else 0)
+            res = max(res, freq + min(ops, total - freq))
+
+        return res
