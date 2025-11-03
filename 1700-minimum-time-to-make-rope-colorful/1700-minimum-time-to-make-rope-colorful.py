3cc45840f027d74cbe68c752f68baceca15fa6ca
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-11-03 07:00:45 +0200

    Time: 130 ms (44.74%), Space: 24.9 MB (89.6%) - LeetHub

--- 1700-minimum-time-to-make-rope-colorful/1700-minimum-time-to-make-rope-colorful.py
+++ 1700-minimum-time-to-make-rope-colorful/1700-minimum-time-to-make-rope-colorful.py
@@ -4,12 +4,6 @@ class Solution:
         ans = 0
         for i in range(1, len(c)):
             if c[i] == c[i - 1]:
-                if nt[i] < nt[i - 1]:
-                    ans += nt[i]
-                    c[i], nt[i] = "*", -1
-                    c[i], c[i - 1] = c[i - 1], c[i]
-                    nt[i], nt[i - 1] = nt[i - 1], nt[i]
-                else:
-                    ans += nt[i - 1]
-                    c[i - 1], nt[i - 1] = "*", -1
+                ans += min(nt[i], nt[i - 1])
+                nt[i] = max(nt[i], nt[i - 1])
         return ans
