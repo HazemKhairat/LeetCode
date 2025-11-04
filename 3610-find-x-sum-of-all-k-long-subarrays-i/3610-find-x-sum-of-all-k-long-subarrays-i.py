827d75d282fa012500201bc0271dd89bec0bb0e0
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-11-04 06:40:52 +0200

    Time: 31 ms (33.77%), Space: 18 MB (15.32%) - LeetHub

--- 3610-find-x-sum-of-all-k-long-subarrays-i/3610-find-x-sum-of-all-k-long-subarrays-i.py
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/3610-find-x-sum-of-all-k-long-subarrays-i.py
@@ -1,17 +1,11 @@
 class Solution:
     def findXSum(self, nums: List[int], k: int, x: int) -> List[int]:
-        i, j = 0, k - 1
+        n = len(nums)
         ans = []
-        while j < len(nums):
-            dic = Counter(nums[i:j+1])
-            dic = sorted(dic.items(), key=lambda x : (-x[1], -x[0]))
-            tmp = x
-            res = 0
-            for l in range(tmp):
-                if l == len(dic): break
-                res += dic[l][0] * dic[l][1]
-            ans.append(res)
-            i += 1
-            j += 1
+        for i in range(n - k + 1):
+            dic = Counter(nums[i : i + k])
+            freq = sorted(dic.items(), key=lambda x: (-x[1], -x[0]))
+            xsum = sum(key * val for key, val in freq[:x])
+            ans.append(xsum)
 
         return ans
