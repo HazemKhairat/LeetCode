2540608c4837a658960cc0bc3d71c68ac2c8b7cc
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-11-19 07:43:08 +0200

    Time: 366 ms (93.39%), Space: 36.2 MB (81.3%) - LeetHub

--- 0307-range-sum-query-mutable/0307-range-sum-query-mutable.py
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.py
@@ -1,50 +1,32 @@
-import math
-
-class SqrtDecomposition:
-    def __init__(self, nums):
-        self.n = len(nums)
-        self.nums = nums
-        self.block_size = int(math.sqrt(self.n))
-        blocks_nums = (self.n + self.block_size - 1) // self.block_size
-        self.blocks = [0] * blocks_nums
-        for i in range(self.n):
-            block_index = i // self.block_size
-            self.blocks[block_index] += nums[i]
-
-    def query(self, l, r):
-        ans = 0
-        while l % self.block_size != 0 and l <= r:
-            ans += self.nums[l]
-            l += 1
-
-        while l + self.block_size - 1 <= r:
-            block_index = l // self.block_size
-            ans += self.blocks[block_index]
-            l += self.block_size
-
-        while l <= r:
-            ans += self.nums[l]
-            l += 1
-
-        return ans
-
-    def update(self, idx, val):
-        block_index = idx // self.block_size
-        diff = val - self.nums[idx]
-        self.nums[idx] = val
-        self.blocks[block_index] += diff
-
-
 class NumArray:
 
     def __init__(self, nums: List[int]):
-        self.sq_d = SqrtDecomposition(nums)
+        self.nums = nums
+        self.n = len(nums)
+        self.bits = [0] * (self.n + 1)
+        for i, num in enumerate(nums, 1):
+            j = i
+            while j <= self.n:
+                self.bits[j] += num
+                j += j & -j
 
     def update(self, index: int, val: int) -> None:
-        self.sq_d.update(index, val)
+        diff = val - self.nums[index]
+        self.nums[index] = val
+        i = index + 1
+        while i <= self.n:
+            self.bits[i] += diff
+            i += i & -i
 
     def sumRange(self, left: int, right: int) -> int:
-        return self.sq_d.query(left, right)
+        def solve(i):
+            total = 0
+            while i > 0:
+                total += self.bits[i]
+                i -= i & -i
+            return total
+
+        return solve(right + 1) - solve(left)
 
 
 # Your NumArray object will be instantiated and called as such:
