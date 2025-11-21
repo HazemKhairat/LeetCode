765e661a2f0ec1f17e25552354eb9a43968e0635
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-11-21 07:26:04 +0200

    Time: 502 ms (49.61%), Space: 18.4 MB (26.6%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.py
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.py
@@ -1,23 +1,11 @@
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
-        s = list(s)
-        start = Counter()
-        for i, ch in enumerate(s):
-            if ch not in start:
-                start[ch] = i
-
-        end = Counter()
-        for i in range(len(s) - 1, -1, -1):
-            ch = s[i]
-            if ch not in end:
-                end[ch] = i
-
+        chars = set(s)
         ans = 0
-        for i in range(97, 123):
-            ch = chr(i)
-            f, e = start[ch], end[ch]
+        for char in chars:
+            i, j = s.index(char), s.rindex(char)
             st = set()
-            for j in range(f + 1, e):
-                st.add(s[j])
+            for k in range(i + 1, j):
+                st.add(s[k])
             ans += len(st)
         return ans
