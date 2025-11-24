1f4bb24f178d4a8278134b9cc9fcca34c6b4e357
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-11-24 06:47:44 +0200

    Time: 91 ms (50.97%), Space: 19.1 MB (66.22%) - LeetHub

--- 1018-binary-prefix-divisible-by-5/1018-binary-prefix-divisible-by-5.py
+++ 1018-binary-prefix-divisible-by-5/1018-binary-prefix-divisible-by-5.py
@@ -1,9 +1,9 @@
 class Solution:
     def prefixesDivBy5(self, nums: List[int]) -> List[bool]:
         n = len(nums)
-        ans = [False] * n
-        tmp = 0
-        for i, num in enumerate(nums):
-            tmp = (tmp << 1) | num
-            ans[i] = tmp % 5 == 0
+        ans = []
+        pref = 0
+        for num in nums:
+            pref = (pref << 1) | num
+            ans.append(pref % 5 == 0)
         return ans
