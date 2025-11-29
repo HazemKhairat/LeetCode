eaeef2ea271b087abdf58f21b44f6ce055541831
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-11-29 09:15:34 +0200

    Time: 102 ms (92.37%), Space: 38.8 MB (61.07%) - LeetHub

--- 2872-maximum-number-of-k-divisible-components/2872-maximum-number-of-k-divisible-components.py
+++ 2872-maximum-number-of-k-divisible-components/2872-maximum-number-of-k-divisible-components.py
@@ -1,27 +1,29 @@
 class Solution:
-    def maxKDivisibleComponents(self, n, edges, values, k):
-        graph = [[] for _ in range(n)]
+    def maxKDivisibleComponents(
+        self, n: int, edges: List[List[int]], values: List[int], k: int
+    ) -> int:
+        graph = [[] for i in range(n)]
         for u, v in edges:
             graph[u].append(v)
             graph[v].append(u)
 
-        count = 0
+        ans = 0
 
-        def dfs(curr, parent):
-            nonlocal count
+        def dfs(node, parent):
+            nonlocal ans
             total = 0
-            for nei in graph[curr]:
-                if nei == parent:
+            for nighbour in graph[node]:
+                if nighbour == parent:
                     continue
-                total += dfs(nei, curr)
+                total += dfs(nighbour, node)
 
-            total += values[curr]
+            total += values[node]
             total %= k
 
             if total == 0:
-                count += 1
+                ans += 1
 
             return total
 
         dfs(0, -1)
-        return count
+        return ans
