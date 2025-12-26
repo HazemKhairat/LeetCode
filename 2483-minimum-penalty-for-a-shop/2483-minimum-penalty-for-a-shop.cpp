0d496fdbb3facdeddac9fa71f45b883972321fda
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-12-26 08:18:46 +0200

    Time: 23 ms (8.2%), Space: 21.9 MB (12.24%) - LeetHub

---
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int bestClosingTime(string c) {
+        int n = c.size();
+        int res = 0;
+        int cost = INT_MAX;
+        vector<int> pref(n + 1, 0), suff(n + 2, 0);
+        
+        for(int i = 1; i <= n; i++){
+            if(c[i - 1] == 'N'){
+                pref[i] = pref[i - 1] + 1;
+            }
+            else{
+                pref[i] = pref[i - 1];
+            }
+        }
+        for(int i = n; i >= 1; i--){
+            if(c[i - 1] == 'Y'){
+                suff[i] = suff[i + 1] + 1;
+            }
+            else{
+                suff[i] = suff[i + 1];
+            }
+            
+        }
+        
+        for(int i = 1; i <= n; i++){
+            if(cost > pref[i - 1] + suff[i]){
+                cost = pref[i - 1] + suff[i];
+                res = i - 1;
+            }
+        }
+        if(pref[n] + suff[n + 1] < cost){
+            return n;
+        }
+        
+        return res;
+    }
+    
+};
