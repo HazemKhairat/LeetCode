0809ec05bac77fb1a763e6636a5221afe647c527
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2025-12-26 15:43:33 +0200

    Time: 1 ms (75.16%), Space: 17.2 MB (71.79%) - LeetHub

--- 3782-last-remaining-integer-after-alternating-deletion-operations/3782-last-remaining-integer-after-alternating-deletion-operations.py
+++ 3782-last-remaining-integer-after-alternating-deletion-operations/3782-last-remaining-integer-after-alternating-deletion-operations.py
@@ -1,14 +1,12 @@
 class Solution:
     def lastInteger(self, n: int) -> int:
- 
-        start, step, leftToRght = 1, 1, True  
+        start, step, rightToLeft = 1, 1, False
 
-        while n > 1:                                
-            if n%2 == 0:
-                if not leftToRght: 
-                    start += step
-            step*= 2
-            n = (n+1)//2
-            leftToRght^= True
+        while n > 1:
+            if n % 2 == 0 and rightToLeft:
+                start += step
+            step *= 2
+            n = (n + 1) // 2
+            rightToLeft ^= True
 
-        return start                                 
+        return start
