312f42619c0d88498e95edae7532ebbde7a12f65
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2026-02-04 13:35:56 +0200

    Time: 50 ms (58.61%), Space: 56.3 MB (71.52%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,26 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function (init) {
+    let tmp = init;
+    return {
+        "increment": () => {
+            return ++tmp;
+        },
+        "reset": () => {
+            tmp = init;
+            return tmp;
+        },
+        "decrement": () => {
+            return --tmp;
+        },
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
