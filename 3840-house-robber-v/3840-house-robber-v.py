5db491ed3761d622521b06fa03a7053354d55eed
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2026-02-21 20:58:33 +0200

    Time: 107 ms (66.57%), Space: 39.7 MB (95.7%) - LeetHub

--- 3840-house-robber-v/3840-house-robber-v.py
+++ 3840-house-robber-v/3840-house-robber-v.py
@@ -2,16 +2,13 @@ class Solution:
     def rob(self, nums: List[int], colors: List[int]) -> int:
 
         n = len(nums)
-        if n == 1:
-            return nums[0]
-
-        dp = [0] * n
-        dp[0] = nums[0]
+        dp = [0] * (n + 1)
+        dp[1] = nums[0]
 
         for i in range(1, n):
             if colors[i] != colors[i - 1]:
-                dp[i] = dp[i - 1] + nums[i]
+                dp[i + 1] = dp[i] + nums[i]
             else:
-                dp[i] = max((dp[i - 2] if i > 1 else 0) + nums[i], dp[i - 1])
+                dp[i + 1] = max(dp[i], dp[i - 1] + nums[i])
 
         return dp[-1]
