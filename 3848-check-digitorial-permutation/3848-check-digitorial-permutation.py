0e8ca96f70388b07dcd268c8177f77ccc4495ca7
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2026-02-27 21:57:23 +0200

    Time: 3 ms (77.57%), Space: 19.2 MB (96.87%) - LeetHub

--- 3848-check-digitorial-permutation/3848-check-digitorial-permutation.py
+++ 3848-check-digitorial-permutation/3848-check-digitorial-permutation.py
@@ -2,32 +2,23 @@ class Solution:
     def isDigitorialPermutation(self, n: int) -> bool:
         fact = [1] * 10
         for i in range(1, 10):
-            fact[i] = i * fact[i -1]
-        
-        arr1 = []
+            fact[i] = i * fact[i - 1]
+
         cnt1 = [0] * 10
-        tmp = n
+        s_n = str(n)
         res = 0
+        for ch in s_n:
+            d = int(ch)
+            cnt1[d] += 1
+            res += fact[d]
 
-        while tmp:
-            arr1.append(tmp % 10)
-            cnt1[tmp%10] += 1
-            res += fact[tmp % 10]
-            tmp //= 10
+        s_res = str(res)
+        if len(s_n) != len(s_res):
+            return False
 
-        arr2 = []
         cnt2 = [0] * 10
+        for ch in s_res:
+            d = int(ch)
+            cnt2[d] += 1
 
-        while res:
-            arr2.append(res % 10)
-            cnt2[res%10] += 1
-            res //= 10
-        # print(arr1)
-        # print(arr2)
-        # print(cnt1)
-        # print(cnt2)
-        if len(arr1) != len(arr2): return False
-        for i in range(len(cnt1)):
-            if cnt1[i] != cnt2[i]:
-                return False
-        return True
+        return cnt1 == cnt2
