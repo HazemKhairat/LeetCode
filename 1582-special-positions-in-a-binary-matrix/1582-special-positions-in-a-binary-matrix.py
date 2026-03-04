0dca48fc7a4a2594de3bcc3a2fada074e524f9c0
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2026-03-04 20:56:40 +0200

    Time: 7 ms (55.34%), Space: 19.7 MB (92.88%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.py
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.py
@@ -2,22 +2,18 @@ class Solution:
     def numSpecial(self, mat: List[List[int]]) -> int:
 
         n, m = len(mat), len(mat[0])
-
-        def solve(r, c):
-            ones = 0
-            for i in range(n):
-                if mat[i][c] == 1:
-                    ones += 1
-            for i in range(m):
-                if mat[r][i] == 1:
-                    ones += 1
-
-            return ones == 2
-
         ans = 0
+        r_cnt = [0] * n
+        c_cnt = [0] * m
+
+        for r in range(n):
+            for c in range(m):
+                r_cnt[r] += mat[r][c]
+                c_cnt[c] += mat[r][c]
+
         for r in range(n):
             for c in range(m):
                 if mat[r][c] == 1:
-                    ans += solve(r, c)
+                    ans += r_cnt[r] == c_cnt[c] == 1
 
         return ans
