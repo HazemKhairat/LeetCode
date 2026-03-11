3bd777c556e2f27d59a53c821294f16a1a60825b
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2026-03-11 20:40:43 +0200

    Time: 382 ms (16.38%), Space: 34.8 MB (13.98%) - LeetHub

---
+++ 3862-find-the-smallest-balanced-index/3862-find-the-smallest-balanced-index.py
@@ -0,0 +1,21 @@
+class Solution:
+    def smallestBalancedIndex(self, nums: list[int]) -> int:
+
+        n = len(nums)
+        prefSum = [0] * (n + 1)
+        suffProd = [1] * (n + 1)
+        MAX = 2**63 - 1
+        for i in range(1, n + 1):
+            prefSum[i] = prefSum[i - 1] + nums[i - 1]
+        for i in range(n - 1, -1, -1):
+            if suffProd[i + 1] > (MAX // nums[i]):
+                suffProd[i] = MAX
+            else:
+                suffProd[i] = suffProd[i + 1] * nums[i]
+        # print(prefSum)
+        # print(suffProd)
+        for i in range(1, n + 1):
+            if prefSum[i - 1] == suffProd[i]:
+                return i - 1
+
+        return -1
