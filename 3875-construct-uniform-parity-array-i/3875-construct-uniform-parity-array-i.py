e1d24a829a118357de6b583c40e8a15fae9cfd63
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2026-03-27 21:02:59 +0200

    Time: 3 ms (16.31%), Space: 19.5 MB (18.24%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.py
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.py
@@ -2,25 +2,14 @@ class Solution:
     def uniformArray(self, nums1: list[int]) -> bool:
 
         n = len(nums1)
-        even = [num for num in nums1 if num % 2 == 0]
-        odd = [num for num in nums1 if num % 2 == 1]
-        
 
         def find(i, parity):
-            pos = -1
-            if parity == 1:
-                if nums1[i] % 2 == 0:
-                    pos = bisect.bisect_right(odd, nums1[i])
-                else:
-                    pos = bisect.bisect_right(even, nums1[i])
-            else:
-                if nums1[i] % 2 == 0:
-                    pos = bisect.bisect_right(odd, nums1[i])
-                else:
-                    pos = bisect.bisect_right(even, nums1[i])
-                    
-            return pos != -1
-                    
+            for j in range(n):
+                if j == i:
+                    continue
+                if (nums1[i] - nums1[j]) % 2 == parity:
+                    return True
+            return False
 
         def solve(parity):
             for i in range(n):
