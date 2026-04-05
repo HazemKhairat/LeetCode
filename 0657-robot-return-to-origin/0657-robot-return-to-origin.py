767d683a48f13cdb6b5dd227cd171167c8edb4e1
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2026-04-05 04:27:09 +0200

    Time: 11 ms (75.07%), Space: 19.2 MB (67.4%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.py
@@ -0,0 +1,14 @@
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        x, y = 0, 0
+        for ch in moves:
+            if ch == 'U':
+                y += 1
+            elif ch == 'D':
+                y -= 1
+            elif ch == 'L':
+                x -= 1
+            else:
+                x += 1
+        
+        return (x, y) == (0, 0)
