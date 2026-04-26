edd58205af2f885f050ea79819741e1896898161
Hazem Khairat Bakri Ali <141364541+HazemKhairat@users.noreply.github.com>
2026-04-26 11:42:46 +0300

    Time: 971 ms (17.16%), Space: 64.8 MB (35.38%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,11 +1,22 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
 
-        dp = [0] * (amount + 1)
-        dp[0] = 1
+        dp = [[None for _ in range(amount + 1)] for _ in range(len(coins))]
 
-        for coin in coins:
-            for i in range(coin, amount + 1):
-                dp[i] += dp[i - coin]
+        def solve(idx, amount):
+            if idx == len(coins) or amount < 0:
+                return 0
+            if amount == 0:
+                return 1
 
-        return dp[-1]
+            if dp[idx][amount] != None:
+                return dp[idx][amount]
+
+            take = skip = 0
+            take = solve(idx, amount - coins[idx])
+            skip = solve(idx + 1, amount)
+
+            dp[idx][amount] = take + skip
+            return take + skip
+
+        return solve(0, amount)
