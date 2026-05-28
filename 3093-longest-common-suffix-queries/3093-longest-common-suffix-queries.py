75246a65327e8641ec124eab72eccc4af066e6aa
Hazem Khairat Bakri Ali <141364541+HazemKhairat@users.noreply.github.com>
2026-05-28 19:38:33 +0300

    Time: 1318 ms (61.01%), Space: 180.4 MB (45.28%) - LeetHub

--- 3093-longest-common-suffix-queries/3093-longest-common-suffix-queries.py
+++ 3093-longest-common-suffix-queries/3093-longest-common-suffix-queries.py
@@ -1,8 +1,8 @@
 class TrieNode:
     def __init__(self):
         self.childs = {}
-        self.min_len = float("inf")
-        self.idx = float("inf")
+        self.min_len = inf
+        self.idx = inf
 
 
 class Trie:
@@ -10,7 +10,9 @@ class Trie:
         self.root = TrieNode()
 
     def insert(self, s, idx):
+
         node = self.root
+
         if len(s) < node.min_len:
             node.min_len = len(s)
             node.idx = idx
@@ -27,6 +29,7 @@ class Trie:
 
     def query(self, s):
         node = self.root
+
         for ch in s:
             if ch in node.childs:
                 node = node.childs[ch]
@@ -40,14 +43,13 @@ class Solution:
     def stringIndices(
         self, wordsContainer: List[str], wordsQuery: List[str]
     ) -> List[int]:
-
         trie = Trie()
-
-        for idx, s in enumerate(wordsContainer):
-            trie.insert(s[::-1], idx)
-
         ans = []
-        for s in wordsQuery:
-            ans.append(trie.query(s[::-1]))
+
+        for i, word in enumerate(wordsContainer):
+            trie.insert(word[::-1], i)
+
+        for q in wordsQuery:
+            ans.append(trie.query(q[::-1]))
 
         return ans
