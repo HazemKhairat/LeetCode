9c9bc36bd6c8c40eabc250b30c789268a1fd8f8e
Hazem Khairat Bakri Ali <141364541+HazemKhairat@users.noreply.github.com>
2026-06-02 09:39:47 +0300

    Time: 2894 ms (80.57%), Space: 78.9 MB (75.1%) - LeetHub

--- 3161-block-placement-queries/3161-block-placement-queries.py
+++ 3161-block-placement-queries/3161-block-placement-queries.py
@@ -1,66 +1,60 @@
 class SegmentTree:
     def __init__(self, size):
-        self.SIZE = size
-        self.tree = [0] * (self.SIZE * 2)
+        self.size = size
+        self.tree = [0] * (self.size * 2)
 
     def update(self, idx, val):
-        idx += self.SIZE
+        idx += self.size
         self.tree[idx] = val
+
         while idx > 1:
             idx //= 2
-            newVal = max(self.tree[2 * idx], self.tree[2 * idx + 1])
+            newVal = max(self.tree[idx * 2 + 1], self.tree[idx * 2])
             if self.tree[idx] != newVal:
                 self.tree[idx] = newVal
             else:
                 return
 
     def query(self, l, r):
-        max_space = 0
-        left = l + self.SIZE
-        right = r + self.SIZE
-
+        left = l + self.size
+        right = r + self.size
+        maxi = -inf
         while left <= right:
-            if left % 2 == 1:
-                max_space = max(max_space, self.tree[left])
-                left += 1
             if right % 2 == 0:
-                max_space = max(max_space, self.tree[right])
+                maxi = max(maxi, self.tree[right])
                 right -= 1
+
+            if left % 2 == 1:
+                maxi = max(maxi, self.tree[left])
+                left += 1
+
             left //= 2
             right //= 2
-        return max_space
+
+        return maxi
 
 
 class Solution:
     def getResults(self, queries: List[List[int]]) -> List[bool]:
-        max_boundary = 50005
-
-        seg_tree = SegmentTree(max_boundary)
-        seg_tree.update(max_boundary - 1, max_boundary - 1)
-
-        obstacles = SortedList([0, max_boundary - 1])
         ans = []
+        seg_tree = SegmentTree(50005)
+        obstcals = SortedList([0, 50001])
+        seg_tree.update(50000, 50000)
 
         for q in queries:
-            if q[0] == 1:
-                x = q[1]
-                next_idx = obstacles.bisect_right(x)
-                nxt = obstacles[next_idx]
-                prev = obstacles[next_idx - 1]
+            op, x = q[0], q[1]
 
-                seg_tree.update(x, x - prev)
+            if op == 1:
+                nxt_idx = obstcals.bisect_right(x)
+                nxt = obstcals[nxt_idx]
+                prev = obstcals[nxt_idx - 1]
                 seg_tree.update(nxt, nxt - x)
-                obstacles.add(x)
+                seg_tree.update(x, x - prev)
+                obstcals.add(x)
             else:
-                x, sz = q[1], q[2]
-
-                next_idx = obstacles.bisect_right(x)
-                prev = obstacles[next_idx - 1]
-
-                max_in_tree = seg_tree.query(0, prev)
-                last_chunk = x - prev
-
-                best_space = max(max_in_tree, last_chunk)
-                ans.append(best_space >= sz)
+                nxt_idx = obstcals.bisect_right(x)
+                prev = obstcals[nxt_idx - 1]
+                max_space = max(seg_tree.query(0, prev), x - prev)
+                ans.append(max_space >= q[2])
 
         return ans
