19778eff136ec924342bed9bf3e67ac2b02268c8
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2026-09-02 11:48:34 +0300

    Time: 6 ms (1.01%), Space: 19.2 MB (47.47%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.py
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.py
@@ -1,18 +1,15 @@
 class Solution:
     def uniformArray(self, nums1: list[int]) -> bool:
         n = len(nums1)
-        nums1 = [num % 2 for num in nums1]
-        cnt = Counter(nums1)
-        nums2 = [None] * n
+        for i in range(n): nums1[i] %= 2
+        
+        cnt = Counter(nums1)  # Space O(1), only 2 values stored: 1 or 0
 
-        def construct(p):
+        def construct(p):  # 0 -> even , 1 -> odd
             for i in range(n):
                 if nums1[i] % 2 != p and cnt[1] - nums1[i] <= 0:
                     return False
 
             return True
 
-        if construct(0) or construct(1):
-            return True
-
-        return False
+        return construct(0) or construct(1)
