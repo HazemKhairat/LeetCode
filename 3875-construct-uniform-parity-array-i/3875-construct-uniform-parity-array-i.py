c526009d75264573c8a22830612d6e450889eb17
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2026-09-02 11:08:09 +0300

    Time: 30 ms (1.01%), Space: 19.4 MB (13.38%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.py
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.py
@@ -3,32 +3,20 @@ class Solution:
         n = len(nums1)
         nums2 = [None] * n
 
-        # even elements construction
-        for i in range(n):
-            if nums1[i] % 2 == 0:
-                nums2[i] = nums1[i]
-            else:
-                for j in range(n):
-                    if i == j:
-                        continue
-                    if (nums1[i] - nums1[j]) % 2 == 0:
-                        nums2[i] = nums1[i] - nums1[j]
+        def construct(p):
+            for i in range(n):
+                if nums1[i] % 2 == p:
+                    nums2[i] = nums1[i]
+                else:
+                    for j in range(n):
+                        if i == j:
+                            continue
+                        if (nums1[i] - nums1[j]) % 2 == p:
+                            nums2[i] = nums1[i] - nums1[j]
 
-        if not any(nums2) == None:
-            return True
+            return not (any(nums2) == 0)
 
-        # odd elements construction
-        for i in range(n):
-            if nums1[i] % 2 == 1:
-                nums2[i] = nums1[i]
-            else:
-                for j in range(n):
-                    if i == j:
-                        continue
-                    if (nums1[i] - nums1[j]) % 2 == 1:
-                        nums2[i] = nums1[i] - nums1[j]
-
-        if not any(num2) == None:
+        if construct(0) or construct(1):
             return True
 
         return False
