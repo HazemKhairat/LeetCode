5e47a4bb349f4fb8701ab7402ffde98eb13528fd
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2026-09-03 11:22:35 +0300

    Time: 263 ms (5.49%), Space: 33.1 MB (100%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
@@ -1,21 +1,25 @@
 class Solution:
     def uniformArray(self, nums1: list[int]) -> bool:
-
         n = len(nums1)
-        odd = [num for num in nums1 if num % 2 == 1]
-        odd.sort()
+        nums1.sort()
+        # actually we only need the parity of the number not the value itself.
+        for i in range(n):
+            nums1[i] %= 2
 
-        def find(i, parity):
-            pos = bisect.bisect_left(odd, nums1[i])
-            if pos == len(odd):
-                return True
-            return (pos != 0) and (nums1[i] != odd[pos])
+        pref = [False] * (n + 1)
+        for i in range(1, n + 1):
+            pref[i] = pref[i - 1] or (nums1[i - 1] == 1)
 
-        def solve(parity):
+        print(pref)
+
+        def construct(p):  # 0 -> even , 1 -> odd
             for i in range(n):
-                if (nums1[i] % 2 != parity) and (not find(i, parity)):
+                # if nums1[i] not similar to the parity
+                # and we can't change the parity using odd element in a prev index
+                # then it's impossible to construct nums2
+                if nums1[i] % 2 != p and not pref[i]:
                     return False
 
             return True
 
-        return solve(0) or solve(1)
+        return construct(0) or construct(1)
