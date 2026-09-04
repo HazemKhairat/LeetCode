1bf2f9a1a48ca5cdd7a494c02c1c33aaebfbee16
Hazem Khairat <141364541+HazemKhairat@users.noreply.github.com>
2026-09-04 12:25:24 +0300

    Time: 2 ms (81.53%), Space: 19.2 MB (71.43%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.py
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.py
@@ -1,17 +1,16 @@
 class Solution:
     def firstStableIndex(self, nums: list[int], k: int) -> int:
         n = len(nums)
-        prefMax = [-inf] * n
-        prefMin = [inf] * n
-        prefMax[0], prefMin[-1] = nums[0], nums[-1]
+        suff_min = [0] * n
+        suff_min[-1] = nums[-1]
+        pref_max = nums[0]
 
-        for i in range(1, n):
-            prefMax[i] = max(prefMax[i - 1], nums[i])
-            prefMin[n - i - 1] = min(prefMin[n - i], nums[n - i - 1])
+        for i in range(n - 2, -1, -1):
+            suff_min[i] = min(suff_min[i + 1], nums[i])
 
         for i in range(n):
-            curr = prefMax[i] - prefMin[i]
-            if curr <= k:
+            pref_max = max(pref_max, nums[i])
+            if pref_max - suff_min[i] <= k:
                 return i
 
         return -1
